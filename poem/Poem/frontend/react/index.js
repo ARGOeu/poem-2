494508799c8e5c0fee0f0e6f8a6005b893d7d666
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-04-08 17:45:02 +0200

    installed bootstrap, reactstrap and css-loader

--- poem/Poem/frontend/react/index.js
+++ poem/Poem/frontend/react/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Aggregation_Profile from './Aggregation_Profile.js';
-import Popup from 'react-popup';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Popup/>, document.getElementById('popupContainer'));
-ReactDOM.render(<Aggregation_Profile django={window.props}/>, document.getElementById('react'));
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(<App />, document.getElementById('react'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- poem/Poem/webpack.config.js
+++ poem/Poem/webpack.config.js
@@ -3,26 +3,34 @@ var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  context: __dirname,
-  entry: "./frontend/react/index.js",
-  output: {
-      path: path.resolve("./frontend/bundles/reactbundle/"),
-      filename: "[name]-[hash].js",
-      chunkFilename: "[name]-[hash].js"
-  },
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
+    context: __dirname,
+    entry: "./frontend/react/index.js",
+    output: {
+        path: path.resolve("./frontend/bundles/reactbundle/"),
+        filename: "[name]-[hash].js",
+        chunkFilename: "[name]-[hash].js"
+    },
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                    plugins: () => [require('autoprefixer')]
+                    }}]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    }
 };
