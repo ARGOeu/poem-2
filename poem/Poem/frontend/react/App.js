bafd467abdba74bb32a46466cedbd3bbc649ae0b
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-04-08 19:47:03 +0200

    basic login card component

--- poem/Poem/frontend/react/App.js
+++ poem/Poem/frontend/react/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Card, CardHeader, CardBody} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Container>
+            <Row>
+                <Col sm={{size: 6, offset: 3}}>
+                    <Card>
+                        <CardHeader>Header</CardHeader>
+                        <CardBody>Body</CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
   }
 }
