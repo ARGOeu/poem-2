f93306baaabdd2fcd1072a7357fd885f269d271b
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-04-10 19:54:40 +0200

    enable svg import

--- poem/Poem/frontend/react/App.js
+++ poem/Poem/frontend/react/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardHeader, CardBody} from 'reactstrap';
+import {
+  Container,
+  Button, 
+  Row, 
+  Col, 
+  Card, 
+  CardHeader, 
+  CardBody,
+  Label,
+  CardFooter,
+  FormGroup} from 'reactstrap';
+import {Formik, Field, Form, ErrorMessage} from 'formik';
+import ArgoLogo from './argologo_color.svg';
+
+import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-        <Container>
-            <Row>
-                <Col sm={{size: 6, offset: 3}}>
-                    <Card>
-                        <CardHeader>Header</CardHeader>
-                        <CardBody>Body</CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row className="login-first-row">
+          <Col sm={{size: 4, offset: 4}}>
+            <Card>
+              <CardHeader className="mt-2">
+                <img alt="logo" src={ArgoLogo}/>
+              </CardHeader>
+              <CardBody>
+                <Formik
+                  initialValues = {{ username: '', password: ''}}
+                  onSubmit = {(values) => alert(JSON.stringify(values, null, 2))}
+                  render = {props => (
+                    <Form>
+                      <FormGroup>
+                        <Label for="username">Username: </Label>
+                        <Field name="username" className="form-control"/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="password">Password: </Label>
+                        <Field name="password" className="form-control"/>
+                      </FormGroup>
+                      <Button className="btn-block" color="primary">Login using username and password</Button>
+                    </Form>
+                  )}
+                >
+                </Formik>
+              </CardBody> 
+              <CardFooter>Footer</CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
