bfbb563854d0ec060a038d8a062d5045ec3c7ab1
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-04-18 11:26:23 +0200

    test logout

--- poem/Poem/frontend/react/Home.js
+++ poem/Poem/frontend/react/Home.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import {
+  Alert,
+  Container,
+  Button, 
+  Row, 
+  Col, 
+  Card, 
+  CardHeader, 
+  CardBody,
+  Label,
+  CardFooter,
+  FormGroup } from 'reactstrap';
+import Cookies from 'universal-cookie'
+
+const doLogout = () =>
+{
+  let cookies = new Cookies();
+
+  return fetch('/rest-auth/logout/', {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRFToken': cookies.get('csrftoken'),
+      'Referer': 'same-origin'
+    }});
+}
 
 const Home = props =>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <Container>
+    <Row>
+      <Col>
+      <h1>Home</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Button color="danger" onClick={doLogout}>Logout</Button>
+    </Row>
+  </Container>
 
 export default Home;
 
