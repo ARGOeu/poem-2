2fa9b47978747897c4e1fc2cfb5f536786e4a4b9
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-05-07 18:17:49 +0200

    have Services as statefull component

--- poem/Poem/frontend/react/Services.js
+++ poem/Poem/frontend/react/Services.js
@@ -14,11 +14,38 @@ import {
   Collapse} from 'reactstrap';
 import {NavigationBar, NavigationLinks} from './UIElements';
 
-const Services = (props) =>
-(
-  <div>
-    "I'm Services"
-  </div>
-)
+
+class Services extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      services: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch('/api/v2/internal/services')
+      .then(response => response.json())
+      .then(json =>
+          this.setState({services: json.tree, loading: false})
+      )
+  }
+
+  render() {
+    const {loading, services} = this.state
+
+    return (
+      (loading)
+      ?
+        <div>Loading...</div>
+      :
+        <div>I'm services</div>
+    )
+
+  }
+}
 
 export default Services;
