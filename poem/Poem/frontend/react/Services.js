fb8c18a575b026bb65b1cc203878b48c78328b31
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-05-08 16:22:32 +0200

    render table with rowspan set

--- poem/Poem/frontend/react/Services.js
+++ poem/Poem/frontend/react/Services.js
@@ -1,19 +1,6 @@
 import React, { Component } from 'react';
-import {
-  Alert,
-  Container,
-  Button, 
-  Row, 
-  Col, 
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarBrand,
-  Navbar,
-  NavbarToggler,
-  Collapse} from 'reactstrap';
-import {NavigationBar, NavigationLinks} from './UIElements';
 
+import './Services.css';
 
 class Services extends Component {
   constructor(props) {
@@ -38,6 +25,16 @@ class Services extends Component {
       )
   }
 
+  getRowSpan(re, match) {
+    var m = re.filter(r => r[0] === match);
+    if (m.length) {
+      return m[0][1]
+    }
+    else {
+      return 1 
+    }
+  }
+
   render() {
     const {loading, rows, rowspan} = this.state
 
@@ -47,34 +44,49 @@ class Services extends Component {
         <div>Loading...</div>
       :
         <table className="table table-bordered table-sm">
-          <thead className="table-info">
+          <thead className="table-active">
             <tr>
-              <th scope="col">Service area</th>
-              <th scope="col">Service name</th>
-              <th scope="col">Service type</th>
-              <th scope="col">Metric</th>
-              <th scope="col">Probe</th>
+              <th id='argo-th' scope="col">Service area</th>
+              <th id='argo-th' scope="col">Service name</th>
+              <th id='argo-th' scope="col">Service type</th>
+              <th id='argo-th' scope="col">Metric</th>
+              <th id='argo-th' scope="col">Probe</th>
             </tr>
           </thead>
           <tbody>
             {
               rows && rows.map((e, i) =>
                 <tr key={i}>
-                  <td className="table-light">
-                    {e.service_area}
-                  </td>
-                  <td className="table-light">
-                    {e.service_name}
-                  </td>
-                  <td className="table-light">
-                    {e.service_type}
-                  </td>
-                  <td className="table-light">
-                    {e.metric}
-                  </td>
-                  <td className="table-light">
-                    {e.probe}
-                  </td>
+                  {
+                    e.service_area && 
+                      <td id='argo-td' className="table-light" rowSpan={this.getRowSpan(rowspan.service_area, e.service_area)}>
+                        {e.service_area}
+                      </td>
+                  }
+                  {
+                    e.service_name && 
+                      <td id='argo-td' className="table-light" rowSpan={this.getRowSpan(rowspan.service_name, e.service_name)}>
+                        {e.service_name}
+                      </td>
+                  }
+                  {
+                    e.service_type && 
+                      <td id='argo-td' className="table-light" rowSpan={this.getRowSpan(rowspan.service_type, e.service_type)}>
+                        {e.service_type}
+                      </td>
+                  }
+                  {
+                    e.metric && 
+                      <td id='argo-td' className="table-light" rowSpan={this.getRowSpan(rowspan.metric, e.metric)}>
+                        {e.metric}
+                      </td>
+                  }
+                  {
+                    e.probe && 
+                      <td id='argo-td' className="table-light" rowSpan={this.getRowSpan(rowspan.probe, e.probe)}>
+                        {e.probe}
+                      </td>
+                  }
                 </tr>
               )
             }
