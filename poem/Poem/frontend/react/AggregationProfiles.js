d255cea2cc66c91f911d935168f08a4288664c3e
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-05-10 18:15:37 +0200

    render table with fetched list of Aggregation profiles

--- poem/Poem/frontend/react/AggregationProfiles.js
+++ poem/Poem/frontend/react/AggregationProfiles.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
-import {
-  Alert,
-  Container,
-  Button, 
-  Row, 
-  Col, 
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarBrand,
-  Navbar,
-  NavbarToggler,
-  Collapse} from 'reactstrap';
-import {NavigationBar, NavigationLinks} from './UIElements';
+import { LoadingAnim } from './UIElements';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 
 class AggregationProfiles extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: false,
+      list_aggregations: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch('/api/v2/internal/aggregations')
+      .then(response => response.json())
+      .then(json =>
+        this.setState({
+          list_aggregations: json, 
+          loading: false})
+      )
   }
 
   render() {
+    const {loading, list_aggregations} = this.state
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Group',
+        accessor: 'groupname'
+      }
+    ]
+
     return (
-      <div>
-        "I'm AggregationProfiles"
-      </div>
+      (loading)
+      ?
+        <LoadingAnim />
+      :
+        list_aggregations && 
+        <ReactTable
+          data={list_aggregations}
+          columns={columns}
+          className="-striped -highlight"
+          defaultPageSize={10}
+        />
     )
   }
 }
