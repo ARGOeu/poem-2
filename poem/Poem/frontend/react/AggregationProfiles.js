f9225cfb2424087f2380b2560a9dcc71c4ea50af
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-05-12 14:18:43 +0200

    split different Aggregation views in separated components

--- poem/Poem/frontend/react/AggregationProfiles.js
+++ poem/Poem/frontend/react/AggregationProfiles.js
@@ -4,9 +4,53 @@ import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
 
+const AggregationProfilesList = ({list_aggregations}) =>
+{
+  const columns = [
+    {
+      Header: 'Name',
+      id: 'name',
+      accessor: e => 
+      <a href={'/ui/aggregationprofiles/change/' + e.apiid}>
+        {e.name}
+      </a>
+    },
+    {
+      Header: 'Group',
+      accessor: 'groupname',
+      maxWidth: 150,
+    }
+  ]
+
+  return (
+    list_aggregations && 
+    <ReactTable
+      data={list_aggregations}
+      columns={columns}
+      className="-striped -highlight"
+      defaultPageSize={10}
+    />
+  )
+}
+
+
 class AggregationProfiles extends Component {
   constructor(props) {
     super(props);
+
+    const {params} = this.props.match;
+    if (params.change) {
+      this.view = 'change';
+    }
+    else if (params.history) {
+      this.view = 'history';
+    }
+    else if (params.add) {
+      this.view = 'add';
+    }
+    else {
+      this.view = undefined;
+    }
 
     this.state = {
       loading: false,
@@ -15,46 +59,29 @@ class AggregationProfiles extends Component {
   }
 
   componentDidMount() {
-    this.setState({loading: true})
-    fetch('/api/v2/internal/aggregations')
-      .then(response => response.json())
-      .then(json =>
-        this.setState({
-          list_aggregations: json, 
-          loading: false})
-      )
+    if (!this.view) {
+      this.setState({loading: true})
+      fetch('/api/v2/internal/aggregations')
+        .then(response => response.json())
+        .then(json =>
+          this.setState({
+            list_aggregations: json, 
+            loading: false})
+        )
+    }
+    else if (this.view == 'change') {
+    }
   }
 
   render() {
     const {loading, list_aggregations} = this.state
-    const columns = [
-      {
-        Header: 'Name',
-        id: 'name',
-        accessor: e => 
-        <a href={'/ui/aggregationprofiles/' + e.apiid}>
-          {e.name}
-        </a>
-      },
-      {
-        Header: 'Group',
-        accessor: 'groupname',
-        maxWidth: 150,
-      }
-    ]
 
     return (
       (loading)
       ?
         <LoadingAnim />
       :
-        list_aggregations && 
-        <ReactTable
-          data={list_aggregations}
-          columns={columns}
-          className="-striped -highlight"
-          defaultPageSize={10}
-        />
+        <AggregationProfilesList list_aggregations={list_aggregations} />
     )
   }
 }
