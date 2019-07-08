6e7bb507d622ddf035afd67c4d70729375b3c141
Katarina Zailac <katarina.zailac@gmail.com>
2019-07-08 13:28:28 +0200

    refactored MetricInstance model so that the table only contains metrics and their service flavours

--- poem/Poem/poem/dbmodels/services.py
+++ poem/Poem/poem/dbmodels/services.py
@@ -20,3 +20,28 @@ class Service(models.Model):
 
     def __str__(self):
         return self.service_name
+
+
+class ServiceFlavour(models.Model):
+    name = models.CharField(max_length=128, help_text='',
+                            verbose_name='Service flavour',
+                            primary_key=True)
+    description = models.CharField(max_length=1024, blank=True, null=True)
+
+    class Meta:
+        app_label = 'poem'
+
+    def __str__(self):
+        return u'%s' % self.name
+
+
+class MetricInstance(models.Model):
+    id = models.AutoField(primary_key=True)
+    service_flavour = models.CharField(max_length=128)
+    metric = models.CharField(max_length=128)
+
+    class Meta:
+        app_label = 'poem'
+
+    def __str__(self):
+        return u'%s %s' % (self.service_flavour, self.metric)
