1053e8870b7f4aa6c3b49776ea328d31c80ad870
Katarina Zailac <katarina.zailac@gmail.com>
2019-07-08 13:31:07 +0200

    removed Profile, GroupOfProfiles and VO models

--- poem/Poem/poem/models.py
+++ poem/Poem/poem/models.py
@@ -1,5 +1,4 @@
 from Poem.poem.dbmodels.probes import *
-from Poem.poem.dbmodels.profiles import *
 from Poem.poem.dbmodels.user import *
 from Poem.poem.dbmodels.metricstags import *
 from Poem.poem.dbmodels.rever import *
