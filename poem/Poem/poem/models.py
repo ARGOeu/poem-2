e60599cc9d3f87019d7a7d41c497f171f3a80c84
Katarina Zailac <katarina.zailac@gmail.com>
2019-07-12 11:18:11 +0200

    removed Probe, GroupOfProbes, and ExtRevision models from poem app

--- poem/Poem/poem/models.py
+++ poem/Poem/poem/models.py
@@ -1,6 +1,4 @@
-from Poem.poem.dbmodels.probes import *
 from Poem.poem.dbmodels.metricstags import *
-from Poem.poem.dbmodels.rever import *
 from Poem.poem.dbmodels.services import *
 from Poem.poem.dbmodels.aggregations import *
 from Poem.poem.dbmodels.metricprofiles import *
