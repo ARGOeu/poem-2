2238b4171d5fcd2c568028ff87fb5c9bdc0794c6
Katarina Zailac <katarina.zailac@gmail.com>
2019-08-09 14:37:37 +0200

    added form to Administration page

--- poem/Poem/frontend/react/Administration.js
+++ poem/Poem/frontend/react/Administration.js
@@ -11,16 +11,68 @@ import {
   NavbarBrand,
   Navbar,
   NavbarToggler,
-  Collapse} from 'reactstrap';
+  Collapse,
+  Card,
+  CardHeader,
+  CardBody} from 'reactstrap';
 import Cookies from 'universal-cookie';
-import {NavigationBar, NavigationLinks} from './UIElements';
+import { NavigationBar, NavigationLinks, Icon } from './UIElements';
+import { Form } from 'formik';
+import { Link } from 'react-router-dom';
 
 
 const Administration = (props) =>
 (
-  <div>
-    "I'm Administration"
-  </div>
+  <Form>
+    <Card>
+      <CardHeader className="mt-2 p-2 text-light text-uppercase rounded" style={{'backgroundColor': "#416090"}}>
+        Poem
+      </CardHeader>
+      <CardBody> 
+        <Row className="p-2 d-flex align-items-center">
+          <Icon i="reports"/> <Link to={'/ui/reports'}>Reports</Link>
+        </Row>
+        <Row className="p-2 d-flex align-items-center">
+          <Icon i="metrics"/> <Link to={'/ui/metrics'}>Metrics</Link>
+        </Row>
+        <Row className="p-2 d-flex align-items-center">
+          <Icon i="metricprofiles"/> <Link to={'/ui/metricprofiles'}>Metric profiles</Link>
+        </Row>
+        <Row className="p-2 d-flex align-items-center">
+          <Icon i="aggregationprofiles"/> <Link to={'/ui/aggregationprofiles'}>Aggregation profiles</Link>
+        </Row>
+      </CardBody>
+    </Card>
+    <Card>
+      <CardHeader className="mt-2 p-2 text-light text-uppercase rounded" style={{'backgroundColor': "#416090"}}>
+        Authentication and authorization
+      </CardHeader>
+      <CardBody> 
+        <Row className="p-2 d-flex align-items-center">
+          <Link to={'/ui/administration/groupofaggregations'}>Groups of aggregations</Link>
+        </Row>
+        <Row className="p-2 d-flex align-items-center">
+          <Link to={'/ui/administration/groupofmetrics'}>Groups of metrics</Link>
+        </Row>
+        <Row className="p-2 d-flex align-items-center">
+          <Link to={'/ui/administration/groupofmetricprofiles'}>Groups of metric profiles</Link>
+        </Row>
+        <Row className="p-2 d-flex align-items-center">
+          <Link to={'/ui/administration/users'}>Users</Link>
+        </Row>
+      </CardBody>
+    </Card>
+    <Card>
+      <CardHeader className="mt-2 p-2 text-light text-uppercase rounded" style={{'backgroundColor': "#416090"}}>
+        API key permissions
+      </CardHeader>
+      <CardBody> 
+        <Row className="p-2 d-flex align-items-center">
+          <Link to={'/ui/administration/apikey'}>API keys</Link>
+        </Row>
+      </CardBody>
+    </Card>
+  </Form>
 )
 
 export default Administration;
