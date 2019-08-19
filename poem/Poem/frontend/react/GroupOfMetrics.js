9b0488ac01515b5fb3d69b6f340e925bc54954b6
Katarina Zailac <katarina.zailac@gmail.com>
2019-08-19 09:52:55 +0200

    created change form for groups of metrics

--- poem/Poem/frontend/react/GroupOfMetrics.js
+++ poem/Poem/frontend/react/GroupOfMetrics.js
@@ -3,6 +3,14 @@ import { Backend } from './DataManager';
 import { LoadingAnim, BaseArgoView } from './UIElements';
 import ReactTable from 'react-table';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import {
+  FormGroup,
+  Row,
+  Col,
+  Label,
+  FormText} from 'reactstrap';
+import FilteredMultiSelect from 'react-filtered-multiselect';
 
 
 export class GroupOfMetricsList extends Component
@@ -63,5 +71,150 @@ export class GroupOfMetricsList extends Component
     }
     else
       return null;
+  }
+}
+
+
+export class GroupOfMetricsChange extends Component {
+  constructor(props) {
+    super(props);
+
+    this.group = props.match.params.group;
+    this.addview = props.addview;
+    this.location = props.location;
+
+    this.state = {
+      name: '',
+      metrics: [],
+      nogroupmetrics: [],
+      write_perm: false,
+      loading: false
+    }
+
+    this.backend = new Backend();
+
+    this.handleDeselect = this.handleDeselect.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleDeselect(deSelectedMetrics) {
+    var metrics = this.state.metrics.slice();
+    var nogroupmetrics = this.state.nogroupmetrics.slice();
+    deSelectedMetrics.forEach(option => {
+      metrics.splice(metrics.indexOf(option), 1);
+    });
+    deSelectedMetrics.forEach(option => {
+      if (nogroupmetrics.indexOf(option) === -1) {
+        nogroupmetrics.push(option)
+      }
+    });
+    this.setState({metrics, nogroupmetrics});
+  }
+
+  handleSelect(metrics) {
+    metrics.sort((a, b) => a.id - b.id)
+    this.setState({metrics})
+  }
+
+  componentDidMount() {
+    this.setState({loading: true});
+
+    if (!this.addview) {
+      Promise.all([this.backend.fetchMetricInGroup(this.group),
+        this.backend.fetchMetricNoGroup()
+      ]).then(([metrics, nogroupmetrics]) => {
+          this.setState({
+            name: this.group,
+            metrics: metrics,
+            nogroupmetrics: nogroupmetrics,
+            write_perm: localStorage.getItem('authIsSuperuser') === 'true',
+            loading: false
+          });
+        });
+    }
+  }
+
+  render() {
+    const { name, metrics, write_perm, loading } = this.state;
+    var nogroupmetrics = this.state.nogroupmetrics;
+    const BOOTSTRAP_CLASSES = {
+      filter: 'form-control',
+      select: 'form-control',
+      button: 'btn btn btn-block btn-default',
+      buttonActive: 'btn btn btn-block btn-primary'
+    }
+
+    if (loading)
+      return(<LoadingAnim/>)
+
+    else if (!loading) {
+      return(
+        <BaseArgoView
+          resourcename='group of metrics'
+          location={this.location}
+          addview={this.addview}
+          modal={true}
+          state={this.state}
+          submitperm={write_perm}>
+            <Formik
+              initialValues={{
+                name: name,
+                metrics: metrics
+              }}
+              render = {props => (
+                <Form>
+                  <FormGroup>
+                    <Row>
+                      <Col md={6}>
+                        <Label for='groupname'>Name</Label>
+                        <Field
+                          type='text'
+                          name='name'
+                          required={true}
+                          className='form-control'
+                          id='groupname'
+                        />
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                  <FormGroup>
+                  <h4 className="mt-2 p-1 pl-3 text-light text-uppercase rounded" style={{"backgroundColor": "#416090"}}>Metrics</h4>
+                    <Row>
+                      <Col md={5}>
+                        <FilteredMultiSelect
+                          buttonText='Add'
+                          classNames={BOOTSTRAP_CLASSES}
+                          onChange={this.handleSelect}
+                          options={nogroupmetrics}
+                          selectedOptions={metrics}
+                          size={15}
+                          textProp='name'
+                          valueProp='id'
+                        />
+                      </Col>
+                      <Col md={5}>
+                        <FilteredMultiSelect
+                          buttonText='Remove'
+                          classNames={{
+                            filter: 'form-control',
+                            select: 'form-control',
+                            button: 'btn btn btn-block btn-default',
+                            buttonActive: 'btn btn btn-block btn-danger'
+                          }}
+                          onChange={this.handleDeselect}
+                          options={metrics}
+                          size={15}
+                          textProp='name'
+                          valueProp='id'
+                        />
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Form>
+              )}
+            />
+          </BaseArgoView>
+      )
+    }
   }
 }
