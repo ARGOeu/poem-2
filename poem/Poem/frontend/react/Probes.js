7c31bcac2b91f349311898f26bdab71d642b6444
Katarina Zailac <katarina.zailac@gmail.com>
2019-08-20 13:40:49 +0200

    created ProbeDetails component

--- poem/Poem/frontend/react/Probes.js
+++ poem/Poem/frontend/react/Probes.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Backend } from './DataManager';
 import { Link } from 'react-router-dom';
-import { LoadingAnim, BaseArgoView } from './UIElements';
+import { LoadingAnim } from './UIElements';
 import ReactTable from 'react-table';
+import { Alert, FormGroup, Label, FormText, Row, Col } from 'reactstrap';
+import { Formik, Form, Field } from 'formik';
 
 
 export class ProbeList extends Component {
@@ -14,7 +16,6 @@ export class ProbeList extends Component {
       list_probe: null
     };
 
-    this.location = props.location;
     this.backend = new Backend();
   }
 
@@ -82,5 +83,171 @@ export class ProbeList extends Component {
     }
     else
       return null;
+  }
+}
+
+
+export class ProbeDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.name = props.match.params.name;
+    this.location = props.location;
+    this.backend = new Backend();
+
+    this.state = {
+      probe: {},
+      loading: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({loading: true});
+
+    this.backend.fetchProbeByName(this.name)
+      .then((json) => {
+        this.setState({
+          probe: json,
+          loading: false
+        });
+      });
+  }
+
+  render() {
+    const { probe, loading } = this.state;
+
+    if (loading)
+      return(<LoadingAnim/>)
+
+    else if (!loading) {
+      return (
+        <React.Fragment>
+          <div className="d-flex align-items-center justify-content-between">
+          <React.Fragment>
+            <h2 className="ml-3 mt-1 mb-4">{'Probe details'}</h2>
+            <Link className='btn btn-secondary' to={this.location.pathname + '/history'} role='button'>History</Link>
+          </React.Fragment>
+          </div>
+          <div id="argo-contentwrap" className="ml-2 mb-2 mt-2 p-3 border rounded">
+            {
+              <Alert color='info'>
+                <center>
+                  This is a read-only instance. Probes can be changed only by super user.
+                </center>
+              </Alert>
+            }
+            <Formik
+              initialValues = {{
+                name: probe.name,
+                version: probe.version,
+                repository: probe.repository,
+                docurl: probe.docurl,
+                description: probe.description,
+                comment: probe.comment
+              }}
+              render = {props => (
+                <Form>
+                  <FormGroup>
+                    <Row>
+                      <Col md={6}>
+                        <Label for='name'>Name</Label>
+                        <Field
+                          type='text'
+                          name='name'
+                          className='form-control'
+                          id='name'
+                        />
+                        <FormText color="muted">
+                          Name of this probe.
+                        </FormText>
+                      </Col>
+                      <Col md={2}>
+                        <Label for='version'>Version</Label>
+                        <Field
+                          type='text'
+                          name='version'
+                          className='form-control'
+                          id='version'
+                        />
+                        <FormText color="muted">
+                          Version of the probe.
+                        </FormText>
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                  <FormGroup>
+                    <h4 className="mt-2 p-1 pl-3 text-light text-uppercase rounded" style={{"backgroundColor": "#416090"}}>Probe metadata</h4>
+                    <Row className='mb-3 align-items-top'>
+                      <Col md={1}>
+                        <Label for='repository'>Repository</Label>
+                      </Col>
+                      <Col md={7}>
+                        <Field
+                          type='text'
+                          name='repository'
+                          className='form-control'
+                          id='repository'
+                        />
+                        <FormText color='muted'>
+                          Probe repository URL.
+                        </FormText>
+                      </Col>
+                    </Row>
+                    <Row className='mb-3 align-items-top'>
+                      <Col md={1}>
+                        <Label for='docurl'>Documentation</Label>
+                      </Col>
+                      <Col md={7}>
+                        <Field
+                          type='text'
+                          name='docurl'
+                          className='form-control'
+                          id='docurl'
+                        />
+                        <FormText color='muted'>
+                          Documentation URL.
+                        </FormText>
+                      </Col>
+                    </Row>
+                    <Row className='mb-3 align-items-top'>
+                      <Col md={1}>
+                        <Label for='description'>Description</Label>
+                      </Col>
+                      <Col md={7}>
+                        <Field
+                          component='textarea'
+                          name='description'
+                          className='form-control'
+                          id='description'
+                        />
+                        <FormText color='muted'>
+                          Free text description outlining the purpose of this probe.
+                        </FormText>
+                      </Col>
+                    </Row>
+                    <Row className='mb-3 align-items-top'>
+                      <Col md={1}>
+                        <Label for='comment'>Comment</Label>
+                      </Col>
+                      <Col md={7}>
+                        <Field 
+                          component='textarea'
+                          name='comment'
+                          className='form-control'
+                          id='comment'
+                        />
+                        <FormText color='muted'>
+                          Short comment about this version.
+                        </FormText>
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                </Form>
+              )}
+            />
+          </div>
+        </React.Fragment>
+      )
+    }
   }
 }
