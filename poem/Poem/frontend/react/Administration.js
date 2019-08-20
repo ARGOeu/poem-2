1a6bfd7c63dc4804209f397903f1342518793244
Katarina Zailac <katarina.zailac@gmail.com>
2019-08-20 11:24:13 +0200

    fixed styling

--- poem/Poem/frontend/react/Administration.js
+++ poem/Poem/frontend/react/Administration.js
@@ -23,41 +23,42 @@ import { Link } from 'react-router-dom';
 
 const Administration = (props) =>
 (
-  <Form>
-    <Card>
-      <CardHeader className="mt-2 p-2 text-light text-uppercase rounded" style={{'backgroundColor': "#416090"}}>
+  <Form className='ml-2 mb-2 mt-2'>
+    <h2 className='ml-3 mt-1 mb-4'>Administration</h2>
+    <Card className='mb-2 pb-1 pt-1'>
+      <CardHeader className="sm-5 p-2 text-light text-uppercase rounded" style={{'backgroundColor': "#416090"}}>
         Poem
       </CardHeader>
-      <CardBody> 
-        <Row className="p-2 d-flex align-items-center">
-          <Icon i="reports"/> <Link to={'/ui/reports'}>Reports</Link>
+      <CardBody>
+        <Row className="p-1 align-items-center">
+          <Icon i="reports"/><Link className='pl-1' to={'/ui/reports'}>Reports</Link>
         </Row>
-        <Row className="p-2 d-flex align-items-center">
-          <Icon i="metrics"/> <Link to={'/ui/metrics'}>Metrics</Link>
+        <Row className="p-1 align-items-center">
+          <Icon i="metrics"/> <Link className='pl-1' to={'/ui/metrics'}>Metrics</Link>
         </Row>
-        <Row className="p-2 d-flex align-items-center">
-          <Icon i="metricprofiles"/> <Link to={'/ui/metricprofiles'}>Metric profiles</Link>
+        <Row className="p-1 align-items-center">
+          <Icon i="metricprofiles"/> <Link className='pl-1' to={'/ui/metricprofiles'}>Metric profiles</Link>
         </Row>
-        <Row className="p-2 d-flex align-items-center">
-          <Icon i="aggregationprofiles"/> <Link to={'/ui/aggregationprofiles'}>Aggregation profiles</Link>
+        <Row className="p-1 align-items-center">
+          <Icon i="aggregationprofiles"/> <Link className='pl-1' to={'/ui/aggregationprofiles'}>Aggregation profiles</Link>
         </Row>
       </CardBody>
     </Card>
-    <Card>
+    <Card className='mb-2'>
       <CardHeader className="mt-2 p-2 text-light text-uppercase rounded" style={{'backgroundColor': "#416090"}}>
         Authentication and authorization
       </CardHeader>
       <CardBody> 
-        <Row className="p-2 d-flex align-items-center">
+        <Row className="p-1 align-items-center">
           <Link to={'/ui/administration/groupofaggregations'}>Groups of aggregations</Link>
         </Row>
-        <Row className="p-2 d-flex align-items-center">
+        <Row className="p-1 align-items-center">
           <Link to={'/ui/administration/groupofmetrics'}>Groups of metrics</Link>
         </Row>
-        <Row className="p-2 d-flex align-items-center">
+        <Row className="p-1 align-items-center">
           <Link to={'/ui/administration/groupofmetricprofiles'}>Groups of metric profiles</Link>
         </Row>
-        <Row className="p-2 d-flex align-items-center">
+        <Row className="p-1 align-items-center">
           <Link to={'/ui/administration/users'}>Users</Link>
         </Row>
       </CardBody>
@@ -67,7 +68,7 @@ const Administration = (props) =>
         API key permissions
       </CardHeader>
       <CardBody> 
-        <Row className="p-2 d-flex align-items-center">
+        <Row className="p-1 align-items-center">
           <Link to={'/ui/administration/apikey'}>API keys</Link>
         </Row>
       </CardBody>
