c88a9bc545f355620a7eaba6d132261df1bca6e8
Katarina Zailac <katarina.zailac@gmail.com>
2019-08-28 09:24:20 +0200

    added route for probe version detail view

--- poem/Poem/frontend/react/App.js
+++ poem/Poem/frontend/react/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Login from './Login';
 import { MetricProfilesChange, MetricProfilesList } from './MetricProfiles';
 import Home from './Home';
-import { ProbeList, ProbeDetails, ProbeHistory, ProbeVersionCompare } from './Probes';
+import { ProbeList, ProbeDetails, ProbeHistory, ProbeVersionCompare, ProbeVersionDetails } from './Probes';
 import { MetricList, MetricChange } from './Metrics';
 import Administration from './Administration';
 import { AggregationProfilesChange, AggregationProfilesList } from './AggregationProfiles';
@@ -158,6 +158,7 @@ class App extends Component {
                   <Route exact path="/ui/probes" component={ProbeList} />
                   <Route exact path="/ui/probes/:name/history" render={props => <ProbeHistory {...props}/>}/>
                   <Route exact path="/ui/probes/:name/history/compare/:id1/:id2" render={props => <ProbeVersionCompare {...props}/>}/>
+                  <Route exact path="/ui/probes/:name/history/:version" render={props => <ProbeVersionDetails {...props}/>}/>
                   <Route exact path="/ui/probes/:name" render={props => <ProbeDetails {...props}/>}/>
                   <Route exact path="/ui/metrics" component={MetricList} />
                   <Route exact path="/ui/metrics/:name" render={props => <MetricChange {...props}/>}/>
