7a9538fae1016a65157eef84d65c2a3177cc5d5f
Katarina Zailac <katarina.zailac@gmail.com>
2019-09-24 14:37:15 +0200

    route for adding new metric template

--- poem/Poem/frontend/react/App.js
+++ poem/Poem/frontend/react/App.js
@@ -132,6 +132,7 @@ const SuperAdminRouteSwitch = ({props}) => (
     <Route exact path="/ui/probes/:name/history/:version" render={props => <ProbeVersionDetails {...props}/>}/>
     <Route exact path="/ui/probes/:name" render={props => <ProbeChange {...props}/>}/>
     <Route exact path='/ui/metrictemplates' component={MetricTemplateList}/>
+    <Route exact path='/ui/metrictemplates/add' render={props => <MetricTemplateChange {...props} addview={true}/>}/>
     <Route exact path='/ui/metrictemplates/:name' render={props => <MetricTemplateChange {...props}/>}/>
     <Route exact path="/ui/administration" component={SuperAdminAdministration}/>
     <Route exact path="/ui/administration/users" component={UsersList} />
