a2fa92892ec04b41e887a425363e26c42776b3c0
Katarina Zailac <katarina.zailac@gmail.com>
2019-10-02 11:24:21 +0200

    created History model in poem_super_admin app

---
+++ poem/Poem/poem_super_admin/dbmodels/history.py
@@ -0,0 +1,15 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class History(models.Model):
+    object_id = models.CharField(max_length=191)
+    serialized_data = models.TextField()
+    object_repr = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True)
+    user = models.CharField(max_length=32)
+
+    class Meta:
+        app_label = 'poem_super_admin'
--- poem/Poem/poem_super_admin/models.py
+++ poem/Poem/poem_super_admin/models.py
@@ -1,3 +1,4 @@
+from Poem.poem_super_admin.dbmodels.history import *
 from Poem.poem_super_admin.dbmodels.metrictemplates import *
 from Poem.poem_super_admin.dbmodels.probes import *
 from Poem.poem_super_admin.dbmodels.rever import *
