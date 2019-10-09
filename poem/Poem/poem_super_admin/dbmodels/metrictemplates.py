8c8c6ee682c32a2e67f5ff9cfbf0023efe6a5a6b
Katarina Zailac <katarina.zailac@gmail.com>
2019-10-09 10:19:17 +0200

    changed probekey field and added unique constraint to name field

--- poem/Poem/poem_super_admin/dbmodels/metrictemplates.py
+++ poem/Poem/poem_super_admin/dbmodels/metrictemplates.py
@@ -1,5 +1,6 @@
 from django.db import models
-from reversion.models import Version
+
+from Poem.poem_super_admin.models import History
 
 
 class MetricTemplateType(models.Model):
@@ -15,10 +16,10 @@ class MetricTemplateType(models.Model):
 
 class MetricTemplate(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     mtype = models.ForeignKey(MetricTemplateType, on_delete=models.CASCADE)
     probeversion = models.CharField(max_length=128)
-    probekey = models.ForeignKey(Version, blank=True, null=True,
+    probekey = models.ForeignKey(History, blank=True, null=True,
                                  on_delete=models.SET_NULL)
     parent = models.CharField(max_length=128)
     probeexecutable = models.CharField(max_length=128)
