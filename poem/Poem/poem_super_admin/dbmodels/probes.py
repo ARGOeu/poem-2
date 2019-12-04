981339bb89490bfe57bbbd0d2105f6d9e1d300df
Katarina Zailac <katarina.zailac@gmail.com>
2019-12-04 14:51:57 +0100

    removed nameversion field from Probe model

--- poem/Poem/poem_super_admin/dbmodels/probes.py
+++ poem/Poem/poem_super_admin/dbmodels/probes.py
@@ -1,14 +1,9 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 
 
 class Probe(models.Model):
-    name = models.CharField(max_length=128, null=False,
-                            help_text='Name of the probe.', unique=True)
-    version = models.CharField(max_length=28, help_text='Version of the probe.')
-    nameversion = models.CharField(max_length=128, null=False,
-                                   help_text='Name, version tuple.')
+    name = models.CharField(max_length=128, null=False, unique=True)
+    version = models.CharField(max_length=28)
     description = models.CharField(max_length=1024)
     comment = models.CharField(max_length=512)
     repository = models.CharField(max_length=512)
@@ -22,9 +17,3 @@ class Probe(models.Model):
 
     def __str__(self):
         return u'%s (%s)' % (self.name, self.version)
-
-
-@receiver(pre_save, sender=Probe)
-def probe_handler(sender, instance, **kwargs):
-    instance.nameversion = u'%s (%s)' % (str(instance.name),
-                                         str(instance.version))
