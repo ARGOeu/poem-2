98ef2428fa0f4a8fc4d1544fd2376946f7905d52
Katarina Zailac <katarina.zailac@gmail.com>
2019-12-06 13:20:34 +0100

    created MetricTemplateHistory model

--- poem/Poem/poem_super_admin/dbmodels/metrictemplates.py
+++ poem/Poem/poem_super_admin/dbmodels/metrictemplates.py
@@ -46,3 +46,36 @@ class MetricTemplate(models.Model):
 
     def __str__(self):
         return u'%s' % self.name
+
+
+class MetricTemplateHistory(models.Model):
+    object_id = models.ForeignKey(MetricTemplate, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    mtype = models.ForeignKey(MetricTemplateType, on_delete=models.CASCADE)
+    probekey = models.ForeignKey(ProbeHistory, null=True,
+                                 on_delete=models.SET_NULL)
+    parent = models.CharField(max_length=128)
+    probeexecutable = models.CharField(max_length=128)
+    config = models.CharField(max_length=1024)
+    attribute = models.CharField(max_length=1024)
+    dependency = models.CharField(max_length=1024)
+    flags = models.CharField(max_length=1024)
+    files = models.CharField(max_length=1024)
+    parameter = models.CharField(max_length=1024)
+    fileparameter = models.CharField(max_length=1024)
+    date_created = models.DateTimeField(auto_now_add=True)
+    version_comment = models.TextField(blank=True)
+    version_user = models.CharField(max_length=32)
+
+    class Meta:
+        app_label = 'poem_super_admin'
+        unique_together = [['name', 'probekey']]
+
+    def __str__(self):
+        if self.probekey:
+            return u'%s [%s]' % (self.name, self.probekey.__str__())
+        else:
+            return u'%s' % self.name
+
+    def natural_key(self):
+        return (self.name, self.probekey)
