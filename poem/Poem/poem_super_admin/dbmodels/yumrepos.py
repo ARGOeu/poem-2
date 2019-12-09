1f822ec98658ae89db2623b83c15fcc87f6a85cc
Katarina Zailac <katarina.zailac@gmail.com>
2019-12-09 09:56:07 +0100

    Package model

--- poem/Poem/poem_super_admin/dbmodels/yumrepos.py
+++ poem/Poem/poem_super_admin/dbmodels/yumrepos.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class PackageManager(models.Manager):
+    def get_by_natural_key(self, name, version):
+        return self.get(name=name, version=version)
+
+
 class YumRepo(models.Model):
     name = models.TextField(unique=True)
     content = models.TextField(blank=True)
@@ -8,3 +13,21 @@ class YumRepo(models.Model):
 
     class Meta:
         app_label = 'poem_super_admin'
+
+
+class Package(models.Model):
+    name = models.TextField(null=False)
+    version = models.TextField(null=False)
+    repo = models.ForeignKey(YumRepo, null=True, on_delete=models.SET_NULL)
+
+    objects = PackageManager()
+
+    class Meta:
+        app_label = 'poem_super_admin'
+        unique_together = [['name', 'version']]
+
+    def __str__(self):
+        return u'%s (%s)' % (self.name, self.version)
+
+    def natural_key(self):
+        return (self.name, self.version)
