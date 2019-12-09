e8f2d3f371fadbcb6d9c6e92a9d5a781a2c4411e
Katarina Zailac <katarina.zailac@gmail.com>
2019-12-09 14:08:34 +0100

    frontent method for fetching packages

--- poem/Poem/frontend/react/DataManager.js
+++ poem/Poem/frontend/react/DataManager.js
@@ -276,6 +276,12 @@ export class Backend {
       .catch(err => alert('Something went wrong: ' + err))
   }
 
+  fetchPackages() {
+    return fetch('/api/v2/internal/packages/')
+      .then(response => response.json())
+      .catch(err => alert('Something went wrong: ' + err))
+  }
+
   changeMetricProfile(profile) {
     return this.send(
       '/api/v2/internal/metricprofiles/',
