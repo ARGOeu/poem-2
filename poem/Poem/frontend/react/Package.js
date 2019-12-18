7f6d4af72e8896818607c247eda213c50e78c327
Katarina Zailac <katarina.zailac@gmail.com>
2019-12-18 12:12:41 +0100

    added separate fields for CentOS 6 and 7 repos

--- poem/Poem/frontend/react/Package.js
+++ poem/Poem/frontend/react/Package.js
@@ -5,7 +5,6 @@ import{ LoadingAnim, BaseArgoView, DropdownFilterComponent, FancyErrorMessage, A
 import ReactTable from 'react-table';
 import {
   FormGroup,
-  Label,
   FormText,
   Row,
   Col,
@@ -24,9 +23,11 @@ const PackageSchema = Yup.object().shape({
   version: Yup.string()
     .matches(/^\S*$/, 'Version cannot contain white spaces')
     .required('Required'),
-  repo: Yup.string()
-    .required('Required')
-});
+  repo_6: Yup.string(),
+  repo_7: Yup.string()
+})
+.test('undefined', 'You must provide at least one repo!', value =>
+!!(value.repo_6 || value.repo_7));
 
 
 export class PackageList extends Component {
@@ -162,9 +163,11 @@ export class PackageChange extends Component {
         id: '',
         name: '',
         version: '',
-        repo: ''
       },
-      list_repos: [],
+      repo_6: '',
+      repo_7: '',
+      list_repos_6: [],
+      list_repos_7: [],
       list_probes: [],
       loading: false,
       write_perm: false,
@@ -221,11 +224,18 @@ export class PackageChange extends Component {
   };
 
   doChange(values, actions) {
+    let repos = [];
+    if (values.repo_6)
+      repos.push(values.repo_6);
+
+    if (values.repo_7)
+      repos.push(values.repo_7);
+   
     if (this.addview) {
       this.backend.addPackage({
         name: values.name,
         version: values.version,
-        repo: values.repo
+        repos: repos
       })
         .then(response => {
           if (!response.ok) {
@@ -246,7 +256,7 @@ export class PackageChange extends Component {
         id: values.id,
         name: values.name,
         version: values.version,
-        repo: values.repo
+        repos: repos
       })
         .then(response => {
           if (!response.ok) {
@@ -269,12 +279,19 @@ export class PackageChange extends Component {
     this.setState({loading: true});
     this.backend.fetchYumRepos()
       .then(repos => {
-          let list_repos = [];
-          repos.forEach(e => list_repos.push(e.name));
+          let list_repos_6 = [];
+          let list_repos_7 = [];
+          repos.forEach(e => {
+            if (e.tag === 'CentOS 6')
+              list_repos_6.push(e.name + ' (' + e.tag + ')');
+            else if (e.tag === 'CentOS 7')
+              list_repos_7.push(e.name + ' (' + e.tag + ')');
+          });
 
           if (this.addview) {
             this.setState({
-              list_repos: list_repos,
+              list_repos_6: list_repos_6,
+              list_repos_7: list_repos_7,
               write_perm: localStorage.getItem('authIsSuperuser') === 'true',
               loading: false
             });
@@ -285,13 +302,27 @@ export class PackageChange extends Component {
             ])
               .then(([pkg, probes]) => {
                 let list_probes = [];
+                let repo_6 = '';
+                let repo_7 = '';
+
+                for (let i = 0; i < pkg.repos.length; i++) {
+                  if (pkg.repos[i].split('(')[1].slice(0, -1) === 'CentOS 6')
+                    repo_6 = pkg.repos[i];
+                  
+                  if (pkg.repos[i].split('(')[1].slice(0, -1) === 'CentOS 7')
+                    repo_7 = pkg.repos[i];
+                }
+
                 probes.forEach(e => {
                   if (e.package === `${pkg.name} (${pkg.version})`)
                     list_probes.push(e.name);
                 });
                 this.setState({
                   pkg: pkg,
-                  list_repos: list_repos,
+                  list_repos_6: list_repos_6,
+                  list_repos_7: list_repos_7,
+                  repo_6: repo_6,
+                  repo_7: repo_7,
                   list_probes: list_probes,
                   write_perm: localStorage.getItem('authIsSuperuser') === 'true',
                   loading: false
@@ -302,12 +333,12 @@ export class PackageChange extends Component {
   };
 
   render() {
-    const { pkg, list_repos, list_probes, write_perm, loading } = this.state;
+    const { pkg, repo_6, repo_7, list_repos_6, list_repos_7, list_probes, write_perm, loading } = this.state;
 
     if (loading)
       return <LoadingAnim/>;
 
-    else if (!loading && list_repos) {
+    else if (!loading && list_repos_6) {
       return (
         <BaseArgoView
           resourcename='package'
@@ -324,7 +355,8 @@ export class PackageChange extends Component {
               id: pkg.id,
               name: pkg.name,
               version: pkg.version,
-              repo: pkg.repo
+              repo_6: repo_6,
+              repo_7: repo_7, 
             }}
             onSubmit = {(values, actions) => this.onSubmitHandle(values, actions)}
             validationSchema={PackageSchema}
@@ -369,24 +401,48 @@ export class PackageChange extends Component {
                       </FormText>
                     </Col>
                   </Row>
+                </FormGroup>
+                <FormGroup>
+                  <h4 className="mt-2 p-1 pl-3 text-light text-uppercase rounded" style={{"backgroundColor": "#416090"}}>Yum repo</h4>
+                  <Row>
+                    <Col md={8}>
+                      <AutocompleteField
+                        {...props}
+                        lists={list_repos_6}
+                        icon='yumrepos'
+                        field='repo_6'
+                        val={props.values.repo_6}
+                        onselect_handler={this.onSelect}
+                        req={props.errors.undefined}
+                        label='CentOS 6 repo'
+                      />
+                      {
+                        props.errors.undefined &&
+                          FancyErrorMessage(props.errors.undefined)
+                      }
+                      <FormText color='muted'>
+                        Package is part of selected CentOS 6 repo.
+                      </FormText>
+                    </Col>
+                  </Row>
                   <Row className='mt-4'>
                     <Col md={8}>
                       <AutocompleteField
                         {...props}
-                        lists={list_repos}
+                        lists={list_repos_7}
                         icon='yumrepos'
-                        field='repo'
-                        val={props.values.repo}
+                        field='repo_7'
+                        val={props.values.repo_7}
                         onselect_handler={this.onSelect}
-                        req={props.errors.repo}
-                        label='Repo'
+                        req={props.errors.undefined}
+                        label='CentOS 7 repo'
                       />
                       {
-                        props.errors.repo &&
-                          FancyErrorMessage(props.errors.repo)
+                        props.errors.undefined &&
+                          FancyErrorMessage(props.errors.undefined)
                       }
                       <FormText color='muted'>
-                        Package is part of selected repo.
+                        Package is part of selected CentOS 7 repo.
                       </FormText>
                     </Col>
                   </Row>
