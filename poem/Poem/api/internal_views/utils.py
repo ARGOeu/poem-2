ed7d4f1207400efac3f4eed0605a58dd480e7b53
Katarina Zailac <katarina.zailac@gmail.com>
2020-02-05 11:05:50 +0100

    refactored sync_webapi function to be able to create/remove history entry for synced metric profiles

--- poem/Poem/api/internal_views/utils.py
+++ poem/Poem/api/internal_views/utils.py
@@ -1,6 +1,10 @@
+from django.contrib.contenttypes.models import ContentType
+
 import json
 
 from Poem.api.models import MyAPIKey
+from Poem.helpers.history_helpers import create_metricprofile_history
+from Poem.poem import models as poem_models
 
 import requests
 
@@ -80,11 +84,33 @@ def sync_webapi(api, model):
     for p in data:
         if p['id'] in entries_not_indb:
             new_entries.append(
-                model(name=p['name'], apiid=p['id'], groupname='')
+                dict(name=p['name'], apiid=p['id'], groupname='')
             )
+
     if new_entries:
-        model.objects.bulk_create(new_entries)
+        for entry in new_entries:
+            instance = model.objects.create(**entry)
+
+            if isinstance(instance, poem_models.MetricProfiles):
+                services = []
+                for item in data:
+                    if item['id'] == instance.apiid:
+                        for service in item['services']:
+                            for metric in service['metrics']:
+                                services.append(
+                                    dict(
+                                        service=service['service'],
+                                        metric=metric
+                                    )
+                                )
+                create_metricprofile_history(instance, services, 'poem')
 
     entries_deleted_onapi = data_db.difference(data_api)
     for p in entries_deleted_onapi:
-        model.objects.get(apiid=p).delete()
+        instance = model.objects.get(apiid=p)
+        poem_models.TenantHistory.objects.filter(
+            object_id=instance.id,
+            content_type=ContentType.objects.get_for_model(model)
+        ).delete()
+        instance.delete()
+
--- poem/Poem/api/tests/test_views_internal.py
+++ poem/Poem/api/tests/test_views_internal.py
@@ -8,6 +8,7 @@ from django.test.client import encode_multipart
 import json
 
 from Poem.api import views_internal as views
+from Poem.api.internal_views.utils import sync_webapi
 from Poem.api.internal_views.utils import inline_metric_for_db
 from Poem.api.models import MyAPIKey
 from Poem.helpers.history_helpers import create_comment, update_comment
@@ -49,6 +50,131 @@ def mocked_inline_metric_for_db(data):
         return json.dumps(result)
     else:
         return ''
+
+
+def mocked_web_api_request(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, data, status_code):
+            self.data = data
+            self.status_code = status_code
+
+        def json(self):
+            return self.data
+
+        def raise_for_status(self):
+            return ''
+
+    if args[0] == 'metric_profiles':
+        return MockResponse(
+            {
+                'data': [
+                    {
+                        'id': '00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+                        'date': '2015-01-01',
+                        'name': 'TEST_PROFILE',
+                        'services': [
+                            {
+                                'service': 'dg.3GBridge',
+                                'metrics': [
+                                    'eu.egi.cloud.Swift-CRUD'
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'id': '11110000-aaaa-kkkk-aaaa-aaeekkccnnee',
+                        'date': '2015-01-01',
+                        'name': 'NEW_PROFILE',
+                        'services': [
+                            {
+                                'service': 'dg.3GBridge',
+                                'metrics': [
+                                    'eu.egi.cloud.Swift-CRUD'
+                                ]
+                            }
+                        ]
+
+                    }
+                ]
+            }, 200
+        )
+
+    if args[0] == 'aggregation_profiles':
+        return MockResponse(
+            {
+                'data': [
+                    {
+                        'id': '00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+                        'date': '2015-01-01',
+                        'name': 'TEST_PROFILE',
+                        'namespace': 'egi',
+                        'endpoint_group': 'sites',
+                        'metric_operation': 'AND',
+                        'profile_operation': 'AND',
+                        'metric_profile': {
+                            'name': 'TEST_PROFILE',
+                            'id': '00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+                        },
+                        'groups': []
+                    },
+                    {
+                        'id': '11110000-aaaa-kkkk-aaaa-aaeekkccnnee',
+                        'date': '2015-01-01',
+                        'name': 'NEW_PROFILE',
+                        'namespace': 'egi',
+                        'endpoint_group': 'sites',
+                        'metric_operation': 'AND',
+                        'profile_operation': 'AND',
+                        'metric_profile': {
+                            'name': 'TEST_PROFILE',
+                            'id': '00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+                        },
+                        'groups': []
+
+                    }
+                ]
+            }, 200
+        )
+
+    if args[0] == 'thresholds_profiles':
+        return MockResponse(
+            {
+                'data': [
+                    {
+                        'id': '00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+                        'date': '2015-01-01',
+                        'name': 'TEST_PROFILE',
+                        'rules': [
+                            {
+                                'metric': 'httpd.ResponseTime',
+                                'thresholds': 'response=20ms;0:300;299:1000'
+                            },
+                            {
+                                'host': 'webserver01.example.foo',
+                                'metric': 'httpd.ResponseTime',
+                                'thresholds': 'response=20ms;0:200;199:300'
+                            },
+                            {
+                                'endpoint_group': 'TEST-SITE-51',
+                                'metric': 'httpd.ResponseTime',
+                                'thresholds': 'response=20ms;0:500;499:1000'
+                            }
+                        ]
+                    },
+                    {
+                        'id': '11110000-aaaa-kkkk-aaaa-aaeekkccnnee',
+                        'date': '2015-01-01',
+                        'name': 'NEW_PROFILE',
+                        'rules': [
+                            {
+                                'metric': 'httpd.ResponseTime',
+                                'thresholds': 'response=20ms;0:300;299:1000'
+                            }
+                        ]
+                    }
+                ]
+            }, 200
+        )
 
 
 class ListAPIKeysAPIViewTests(TenantTestCase):
@@ -1794,9 +1920,9 @@ class ListMetricProfilesAPIViewTests(TenantTestCase):
         )
         data1[0]['fields'].update({
             'metricinstances': [
-                ('AMGA', 'org.nagios.SAML-SP'),
-                ('APEL', 'org.apel.APEL-Pub'),
-                ('APEL', 'org.apel.APEL-Sync')
+                ['AMGA', 'org.nagios.SAML-SP'],
+                ['APEL', 'org.apel.APEL-Pub'],
+                ['APEL', 'org.apel.APEL-Sync']
             ]
         })
 
@@ -7863,3 +7989,168 @@ class ListMetricTemplatesForImportTests(TenantTestCase):
                 }
             ]
         )
+
+
+class SyncWebApiTests(TenantTestCase):
+    def setUp(self):
+        ct_mp = ContentType.objects.get_for_model(poem_models.MetricProfiles)
+        MyAPIKey.objects.create(
+            name='WEB-API',
+            token='mocked_token'
+        )
+
+        self.mp1 = poem_models.MetricProfiles.objects.create(
+            name='TEST_PROFILE',
+            apiid='00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='EGI'
+        )
+
+        self.mp2 = poem_models.MetricProfiles.objects.create(
+            name='ANOTHER-PROFILE',
+            apiid='12341234-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='EGI'
+        )
+
+        data = json.loads(
+            serializers.serialize(
+                'json', [self.mp1],
+                use_natural_foreign_keys=True,
+                use_natural_primary_keys=True
+            )
+        )
+        data[0]['fields'].update({
+            'metricinstances': [
+                ['AMGA', 'org.nagios.SAML-SP'],
+                ['APEL', 'org.apel.APEL-Pub'],
+                ['APEL', 'org.apel.APEL-Sync']
+            ]
+        })
+
+        poem_models.TenantHistory.objects.create(
+            object_id=self.mp1.id,
+            serialized_data=json.dumps(data),
+            object_repr=self.mp1.__str__(),
+            comment='Initial version.',
+            user='testuser',
+            content_type=ct_mp
+        )
+
+        data = json.loads(
+            serializers.serialize(
+                'json', [self.mp2],
+                use_natural_foreign_keys=True,
+                use_natural_primary_keys=True
+            )
+        )
+        data[0]['fields'].update({
+            'metricinstances': [
+                ['AMGA', 'org.nagios.SAML-SP'],
+                ['APEL', 'org.apel.APEL-Pub'],
+                ['APEL', 'org.apel.APEL-Sync']
+            ]
+        })
+
+        poem_models.TenantHistory.objects.create(
+            object_id=self.mp2.id,
+            serialized_data=json.dumps(data),
+            object_repr=self.mp2.__str__(),
+            comment='Initial version.',
+            user='testuser',
+            content_type=ct_mp
+        )
+
+        self.aggr1 = poem_models.Aggregation.objects.create(
+            name='TEST_PROFILE',
+            apiid='00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='EGI'
+        )
+
+        self.aggr2 = poem_models.Aggregation.objects.create(
+            name='ANOTHER-PROFILE',
+            apiid='12341234-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='EGI'
+        )
+
+        self.tp1 = poem_models.ThresholdsProfiles.objects.create(
+            name='TEST_PROFILE',
+            apiid='00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='EGI'
+        )
+
+        self.tp2 = poem_models.ThresholdsProfiles.objects.create(
+            name='ANOTHER-PROFILE',
+            apiid='12341234-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='EGI'
+        )
+
+    @patch('requests.get', side_effect=mocked_web_api_request)
+    def test_sync_webapi_metricprofiles(self, func):
+        self.assertEqual(poem_models.MetricProfiles.objects.all().count(), 2)
+        sync_webapi('metric_profiles', poem_models.MetricProfiles)
+        self.assertEqual(poem_models.MetricProfiles.objects.all().count(), 2)
+        self.assertEqual(
+            poem_models.MetricProfiles.objects.get(name='TEST_PROFILE'),
+            self.mp1
+        )
+        self.assertRaises(
+            poem_models.MetricProfiles.DoesNotExist,
+            poem_models.MetricProfiles.objects.get,
+            name='ANOTHER-PROFILE'
+        )
+        self.assertEqual(
+            poem_models.TenantHistory.objects.filter(
+                object_id=self.mp2.id
+            ).count(), 0
+        )
+        self.assertTrue(
+            poem_models.MetricProfiles.objects.get(name='NEW_PROFILE')
+        )
+        history = poem_models.TenantHistory.objects.filter(
+            object_repr='NEW_PROFILE'
+        )
+        self.assertEqual(history.count(), 1)
+        self.assertEqual(history[0].comment, 'Initial version.')
+        serialized_data = json.loads(history[0].serialized_data)[0]['fields']
+        self.assertEqual(serialized_data['name'], 'NEW_PROFILE')
+        self.assertEqual(
+            serialized_data['apiid'], '11110000-aaaa-kkkk-aaaa-aaeekkccnnee'
+        )
+        self.assertEqual(
+            serialized_data['metricinstances'],
+            [['dg.3GBridge', 'eu.egi.cloud.Swift-CRUD']]
+        )
+
+    @patch('requests.get', side_effect=mocked_web_api_request)
+    def test_sync_webapi_aggregationprofiles(self, func):
+        self.assertEqual(poem_models.Aggregation.objects.all().count(), 2)
+        sync_webapi('aggregation_profiles', poem_models.Aggregation)
+        self.assertEqual(poem_models.Aggregation.objects.all().count(), 2)
+        self.assertEqual(
+            poem_models.Aggregation.objects.get(name='TEST_PROFILE'), self.aggr1
+        )
+        self.assertRaises(
+            poem_models.Aggregation.DoesNotExist,
+            poem_models.Aggregation.objects.get,
+            name='ANOTHER-PROFILE'
+        )
+        self.assertTrue(poem_models.Aggregation.objects.get(name='NEW_PROFILE'))
+
+    @patch('requests.get', side_effect=mocked_web_api_request)
+    def test_sync_webapi_thresholdsprofile(self, func):
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        sync_webapi('thresholds_profiles', poem_models.ThresholdsProfiles)
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.get(name='TEST_PROFILE'),
+            self.tp1
+        )
+        self.assertRaises(
+            poem_models.ThresholdsProfiles.DoesNotExist,
+            poem_models.ThresholdsProfiles.objects.get,
+            name='ANOTHER-PROFILE'
+        )
+
