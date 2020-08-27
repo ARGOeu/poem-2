811383fa6fcadf72f7b97e2f259c9707b09f5a8c
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-08-27 16:19:37 +0200

    inital fetch of Service Types descriptions from backend

--- poem/Poem/frontend/react/ServiceTypes.js
+++ poem/Poem/frontend/react/ServiceTypes.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Backend } from './DataManager';
+import { LoadingAnim, ErrorComponent } from './UIElements';
 
+export const ServiceTypesList = (props) => {
+  const location = props.location;
+  const backend = new Backend();
+  const [loading, setLoading] = useState(false);
+  const [serviceTypesDescriptions, setServiceTypesDescriptions] = useState([]);
+  const [error, setError] = useState(null);
 
-export const ServiceTypesList = () => {
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        let json = await backend.fetchData('/api/v2/internal/servicetypesdesc');
+        setServiceTypesDescriptions(json);
+        setLoading(false);
+      }
+      fetchData();
+    }
+    catch (err) {
+      setError(err)
+      setLoading(false)
+    }
+  }, [])
+
+  if (loading)
+    return (<LoadingAnim/>);
+
+  else if (error)
+    return (<ErrorComponent error={error}/>);
+
   return (
     "I'm ServiceTypes"
   )
