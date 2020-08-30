0e4002124ec801c17e673c8dc6abd822bcf1b3f7
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-08-30 09:03:55 +0200

    initial pagination

--- poem/Poem/frontend/react/ServiceTypes.js
+++ poem/Poem/frontend/react/ServiceTypes.js
@@ -1,47 +1,100 @@
 import React, { useState, useEffect } from 'react';
 import { Backend } from './DataManager';
 import { LoadingAnim, BaseArgoView, ErrorComponent } from './UIElements';
-import { useTable } from 'react-table';
+import { useTable, usePagination } from 'react-table';
+import {
+  Pagination,
+  PaginationItem,
+  PaginationLink
+} from 'reactstrap';
+
 
 function Table({ columns, data }) {
   // Use the state and functions returned from useTable to build your UI
   const {
     headerGroups,
-    rows,
     prepareRow,
-  } = useTable({
-    columns,
-    data,
-  })
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 }
+    },
+    usePagination
+  )
 
   // Render the UI for your table
   return (
-    <table className="table table-bordered table-sm table-hover">
-      <thead className="table-active align-middle text-center">
-        {headerGroups.map((headerGroup, thi) => (
-          <tr key={thi}>
-            {headerGroup.headers.map((column, tri) => (
-              <th key={tri}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {rows.map((row, row_index) => {
-          prepareRow(row)
-          return (
-            <tr key={row_index}>
-              {row.cells.map((cell, cell_index) => {
-                if (cell_index === 0)
-                  return <td key={cell_index} className="align-middle text-center table-light">{row_index + 1}</td>
-                else
-                  return <td key={cell_index} className="align-middle table-light">{cell.render('Cell')}</td>
-              })}
+    <React.Fragment>
+      <table className="table table-bordered table-sm table-hover">
+        <thead className="table-active align-middle text-center">
+          {headerGroups.map((headerGroup, thi) => (
+            <tr key={thi}>
+              {headerGroup.headers.map((column, tri) => (
+                <th key={tri}>{column.render('Header')}</th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody>
+          {page.map((row, row_index) => {
+            prepareRow(row)
+            return (
+              <tr key={row_index}>
+                {row.cells.map((cell, cell_index) => {
+                  if (cell_index === 0)
+                    return <td key={cell_index} className="align-middle text-center table-light">{row_index + 1}</td>
+                  else
+                    return <td key={cell_index} className="align-middle table-light">{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <Pagination>
+        <PaginationItem>
+          <PaginationLink first onClick={() => gotoPage(0)} disabled={!canPreviousPage}/>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink previous onClick={() => previousPage()} disabled={!canPreviousPage}/>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink next onClick={() => nextPage()} disabled={!canNextPage}/>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink last onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}/>
+        </PaginationItem>
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </Pagination>
+    </React.Fragment>
   )
 }
 
