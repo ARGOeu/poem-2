896fdaa75fd71330808e4b54372a7084339f4b20
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-08-31 10:14:23 +0200

    consistent colors and hover as with other tables

--- poem/Poem/frontend/react/ServiceTypes.js
+++ poem/Poem/frontend/react/ServiceTypes.js
@@ -114,9 +114,9 @@ function Table({ columns, data }) {
                   <tr key={row_index}>
                     {row.cells.map((cell, cell_index) => {
                       if (cell_index === 0)
-                        return <td key={cell_index} className="align-middle text-center table-light">{(row_index + 1) + (pageIndex * pageSize)}</td>
+                        return <td key={cell_index} className="align-middle text-center">{(row_index + 1) + (pageIndex * pageSize)}</td>
                       else
-                        return <td key={cell_index} className="align-middle table-light">{cell.render('Cell')}</td>
+                        return <td key={cell_index} className="align-middle">{cell.render('Cell')}</td>
                     })}
                   </tr>
                 )
