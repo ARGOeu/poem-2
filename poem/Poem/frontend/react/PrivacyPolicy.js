acceb59008e7f1a44d3a8cc19ff9b1090103f723
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-09-09 18:32:17 +0200

    added "How long are cookies stored on your device"

--- poem/Poem/frontend/react/PrivacyPolicy.js
+++ poem/Poem/frontend/react/PrivacyPolicy.js
@@ -12,11 +12,15 @@ export const PrivacyPolicy = (props) =>
     <h3 className='p-2'>Cookie Policies</h3>
     <hr/>
 
-    <h4 className='p-2'>1. Purpose of this cookie policy</h4>
+    <h4 className='p-3'>1. Purpose of this cookie policy</h4>
     <p>
-      This Website uses technologies such as "cookies" and "pixel tags". By browsing the webpage, the User agrees to the creation and use of cookies.<br/>
-      If User does not wish to consent to the use of cookies, User may either disable them or discontinue browsing this webpage.<br/>
-      For the purpose of this Policy, the following terms shall have the following meaning:<br/>
+      This Website uses technologies such as "cookies" and "pixel tags". By
+      browsing the webpage, the User agrees to the creation and use of
+      cookies.<br/>
+      If User does not wish to consent to the use of cookies, User may either
+      disable them or discontinue browsing this webpage.<br/>
+      For the purpose of this Policy, the following terms shall have the
+      following meaning:<br/>
     </p>
     <ul>
       <li>
@@ -26,18 +30,25 @@ export const PrivacyPolicy = (props) =>
         placed by other websites, and they may not harm or damage your system.
       </li>
       <li>
-        "Persistence cookies" remain during multiple website visits and get stored on your hard disk.
+        "Persistence cookies" remain during multiple website visits and get
+        stored on your hard disk.
       </li>
       <li>
-        "Session Cookies" are temporary cookies that disappear automatically after you leave a website;
+        "Session Cookies" are temporary cookies that disappear automatically
+        after you leave a website;
       </li>
       <li>
-        "Third Party Cookies" are cookies used by the websites of our partners, as integrated in our own Website or used by websites we link to.
+        "Third Party Cookies" are cookies used by the websites of our partners,
+        as integrated in our own Website or used by websites we link to.
       </li>
     </ul>
-    <h4 className='p-2'>2. Which cookies are placed on your device when using our Website?</h4>
+
+    <h4 className='p-3'>2. Which cookies are placed on your device when using our Website?</h4>
     <p>
-      When you access and/or use the Website, we place one or more Cookies on your device for the purposes described herein. The following table and relevant information set out the cookies used for the Website.It also provides details of third parties setting cookies:
+      When you access and/or use the Website, we place one or more Cookies on
+      your device for the purposes described herein. The following table and
+      relevant information set out the cookies used for the Website. It also
+      provides details of third parties setting cookies:
     </p>
     <Table responsive>
       <thead>
@@ -61,4 +72,14 @@ export const PrivacyPolicy = (props) =>
         </tr>
       </tbody>
     </Table>
+
+    <h4 className='p-3'>3. How long are cookies stored on your device?</h4>
+      <p>
+        The duration for which a cookie will be stored on your browsing device
+        depends on whether it is a "persistence" or a "session" cookie. Session
+        cookies will be stored on a device until you turn off your web
+        browser.<br/>
+        "Persistence cookies" shall remain on your device after you have finished
+        browsing until they expire or until they are deleted by you.
+      </p>
   </BaseArgoView>
