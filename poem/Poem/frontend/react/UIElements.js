b05110aaaf0e00cf1c156df826d86daf8bfdbe41
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-09-11 17:21:23 +0200

    introduce basic Table component for metric profiles

--- poem/Poem/frontend/react/UIElements.js
+++ poem/Poem/frontend/react/UIElements.js
@@ -72,7 +72,7 @@ import { CookiePolicy } from './CookiePolicy';
 
 
 var list_pages = ['administration', 'probes',
-                  'metrics', 'reports', servicetypes', 'metricprofiles', 'aggregationprofiles',
+                  'metrics', 'reports', 'servicetypes', 'metricprofiles', 'aggregationprofiles',
                   'thresholdsprofiles', 'operationsprofiles'];
 var admin_list_pages = [ 'administration', 'tenants',
                         'yumrepos', 'packages', 'probes', 'metrictemplates'];
