2bd36da8d0053d93a65095fffbe705c8e1cf5b23
Katarina Zailac <katarina.zailac@gmail.com>
2020-09-21 15:01:13 +0200

    refactored 'OperationsProfilesList' to use react-table-v7

--- poem/Poem/frontend/react/OperationsProfiles.js
+++ poem/Poem/frontend/react/OperationsProfiles.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { WebApi } from './DataManager';
 import { Link } from 'react-router-dom';
 import { LoadingAnim, ErrorComponent, BaseArgoView, ParagraphTitle } from './UIElements';
-import ReactTable from 'react-table-6';
+import { useTable, usePagination } from 'react-table';
 import {
   FormGroup,
   Row,
@@ -10,16 +10,143 @@ import {
   InputGroup,
   InputGroupAddon,
   FormText,
-  Table
+  Table,
+  Pagination,
+  PaginationItem,
+  PaginationLink
 } from 'reactstrap';
 import { Formik, Form, Field } from 'formik';
+import { useQuery } from 'react-query';
+
+
+function OperationsProfilesListTable({ columns, data }) {
+  const {
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 }
+    },
+    usePagination
+  );
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <Table className='table table-bordered table-hover'>
+            <thead className='table-active align-middle text-center'>
+              {
+                headerGroups.map((headerGroup, thi) => (
+                  <React.Fragment key={thi}>
+                    <tr>
+                      {
+                        headerGroup.headers.map((column, tri) => {
+                          let width = undefined;
+
+                          if (tri === 0)
+                            width = '2%'
+
+                          if (tri === 1)
+                            width = '20%'
+
+                          else if (tri === 2)
+                            width = '78%'
+
+                          return (
+                            <th style={{width: width}} className='p-1 m-1' key={tri}>
+                              {column.render('Header')}
+                            </th>
+                          )
+                        })
+                      }
+                    </tr>
+                  </React.Fragment>
+                ))
+              }
+            </thead>
+            <tbody>
+              {
+                page.map((row, row_index) => {
+                  prepareRow(row);
+                  return (
+                    <tr key={row_index} style={{height: '49px'}}>
+                      {
+                        row.cells.map((cell, cell_index) => {
+                          if (cell_index === 0)
+                            return <td key={cell_index} className='align-middle text-center'>{(row_index + 1) + (pageIndex * pageSize)}</td>
+                          else if (cell_index === row.cells.length - 1)
+                            return <td key={cell_index} className='align-middle text-center'>{cell.render('Cell')}</td>
+                          else
+                            return <td key={cell_index} className='align-middle'>{cell.render('Cell')}</td>
+                        })
+                      }
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'>
+          <Pagination>
+            <PaginationItem disabled={!canPreviousPage}>
+              <PaginationLink first onClick={() => gotoPage(0)}/>
+            </PaginationItem>
+            <PaginationItem disabled={!canPreviousPage}>
+              <PaginationLink previous onClick={() => previousPage()}/>
+            </PaginationItem>
+            {
+              [...Array(pageCount)].map((e, i) =>
+                <PaginationItem key={i} active={pageIndex === i ? true : false}>
+                  <PaginationLink onClick={() => gotoPage(i)}>
+                    { i + 1 }
+                  </PaginationLink>
+                </PaginationItem>
+              )
+            }
+            <PaginationItem disabled={!canNextPage}>
+              <PaginationLink next onClick={() => nextPage()}/>
+            </PaginationItem>
+            <PaginationItem disabled={!canNextPage}>
+              <PaginationLink last onClick={() => gotoPage(pageCount + 1)}/>
+            </PaginationItem>
+            <PaginationItem className='pl-2'>
+              <select
+                style={{width: '180px'}}
+                className='custom-select text-primary'
+                value={pageSize}
+                onChange={e => setPageSize(Number(e.target.value))}
+              >
+                {[10, 20, 50].map(pageSize => (
+                  <option key={pageSize} value={pageSize}>
+                    {pageSize} operations profiles
+                  </option>
+                ))}
+              </select>
+            </PaginationItem>
+          </Pagination>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 
 export const OperationsProfilesList = (props) => {
-  const [loading, setLoading] = useState(false);
-  const [listProfiles, setListProfiles] = useState(null);
-  const [error, setError] = useState(null);
-
   const location = props.location;
   const publicView = props.publicView;
   const webapi = new WebApi({
@@ -27,21 +154,23 @@ export const OperationsProfilesList = (props) => {
     operationsProfiles: props.webapioperations
   });
 
-  useEffect(() => {
-    setLoading(true);
-    async function fetchData() {
-      try {
-          let json = await webapi.fetchOperationsProfiles();
-          setListProfiles(json);
-      } catch(err) {
-        setError(err);
-      };
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+  const { data: listProfiles, error: error, isLoading: loading } = useQuery(
+    'operationsprofiles_listview', async () => {
+      let profiles = await webapi.fetchOperationsProfiles();
+      let n_elem = 10 - (profiles.length % 10);
 
-  const columns = [
+      for (let i = 0; i < n_elem; i++)
+        profiles.push({'name': '', 'description': ''});
+
+      return profiles;
+    }
+  );
+
+  const columns = React.useMemo(() => [
+    {
+      Header: '#',
+      accessor: null
+    },
     {
       Header: 'Name',
       id: 'name',
@@ -49,8 +178,12 @@ export const OperationsProfilesList = (props) => {
         <Link to={`/ui/${publicView ? 'public_' : ''}operationsprofiles/${e.name}`}>
           {e.name}
         </Link>
+    },
+    {
+      Header: 'Description',
+      accessor: 'description'
     }
-  ];
+  ]);
 
   if (loading)
     return (<LoadingAnim/>);
@@ -66,16 +199,12 @@ export const OperationsProfilesList = (props) => {
         listview={true}
         addnew={false}
       >
-        <ReactTable
+        <OperationsProfilesListTable
           data={listProfiles}
           columns={columns}
-          className='-highlight'
-          defaultPageSize={12}
-          rowsText='profiles'
-          getTheadThProps={() => ({className: 'table-active font-weight-bold p-2'})}
         />
       </BaseArgoView>
-    )
+    );
 
   else
     return null;
