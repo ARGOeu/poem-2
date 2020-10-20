f69e8e7dc731ec5817ee92bfaa353e57ccbc044b
Katarina Zailac <katarina.zailac@gmail.com>
2020-10-20 13:21:45 +0200

    configuration file for eslint

---
+++ poem/Poem/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    parser: "babel-eslint",
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended" ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-hooks",
+         "@getify/proper-arrows"
+    ],
+    "rules": {
+        "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+        "react/prop-types": [0],
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/display-name": [0, { "ignoreTranspilerName": false }],
+        "react/jsx-indent": [2, 2, {checkAttributes: true}],
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+        "@getify/proper-arrows/params": ["error", {"unused": "all", "count": 10, "length": 4, "allowed": ["e", "err", "i", "id", "a", "b"], "trivial": false}],
+    }
+};
