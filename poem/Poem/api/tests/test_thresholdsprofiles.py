cd6e35c188dcd1fd2d46ed4ee85a85811714c683
Katarina Zailac <katarina.zailac@gmail.com>
2020-11-05 14:10:04 +0100

    move tests regarding thresholds profiles to a separate file

---
+++ poem/Poem/api/tests/test_thresholdsprofiles.py
@@ -0,0 +1,379 @@
+import json
+from collections import OrderedDict
+from unittest.mock import patch
+
+from Poem.api import views_internal as views
+from Poem.poem import models as poem_models
+from Poem.users.models import CustUser
+from django.contrib.contenttypes.models import ContentType
+from django.core import serializers
+from django.test.client import encode_multipart
+from rest_framework import status
+from rest_framework.test import force_authenticate
+from tenant_schemas.test.cases import TenantTestCase
+from tenant_schemas.test.client import TenantRequestFactory
+
+
+def encode_data(data):
+    content = encode_multipart('BoUnDaRyStRiNg', data)
+    content_type = 'multipart/form-data; boundary=BoUnDaRyStRiNg'
+
+    return content, content_type
+
+
+def mocked_func(*args, **kwargs):
+    pass
+
+
+class ListThresholdsProfilesAPIViewTests(TenantTestCase):
+    def setUp(self):
+        self.factory = TenantRequestFactory(self.tenant)
+        self.view = views.ListThresholdsProfiles.as_view()
+        self.url = '/api/v2/internal/thresholdsprofiles/'
+        self.user = CustUser.objects.create_user(username='testuser')
+
+        self.tp1 = poem_models.ThresholdsProfiles.objects.create(
+            name='TEST_PROFILE',
+            apiid='00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+            groupname='GROUP'
+        )
+
+        self.tp2 = poem_models.ThresholdsProfiles.objects.create(
+            name='ANOTHER_PROFILE',
+            apiid='12341234-oooo-kkkk-aaaa-aaeekkccnnee'
+        )
+
+        poem_models.GroupOfThresholdsProfiles.objects.create(name='GROUP')
+        poem_models.GroupOfThresholdsProfiles.objects.create(name='NEWGROUP')
+
+        self.ct = ContentType.objects.get_for_model(
+            poem_models.ThresholdsProfiles
+        )
+
+        data = json.loads(
+            serializers.serialize(
+                'json', [self.tp1],
+                use_natural_foreign_keys=True,
+                use_natural_primary_keys=True
+            )
+        )
+        data[0]['fields'].update({
+            'rules': [
+                {
+                    'host': 'hostFoo',
+                    'metric': 'metricA',
+                    'thresholds': 'freshness=1s;10;9:;0;25 entries=1;3;0:2;10'
+                }
+            ]
+        })
+
+        poem_models.TenantHistory.objects.create(
+            object_id=self.tp1.id,
+            serialized_data=json.dumps(data),
+            object_repr=self.tp1.__str__(),
+            comment='Initial version.',
+            user='testuser',
+            content_type=self.ct
+        )
+
+        data = json.loads(
+            serializers.serialize(
+                'json', [self.tp2],
+                use_natural_foreign_keys=True,
+                use_natural_primary_keys=True
+            )
+        )
+        data[0]['fields'].update({
+            'rules': [
+                {
+                    'metric': 'metricB',
+                    'thresholds': 'freshness=1s;10;9:;0;25'
+                }
+            ]
+        })
+
+        poem_models.TenantHistory.objects.create(
+            object_id=self.tp2.id,
+            serialized_data=json.dumps(data),
+            object_repr=self.tp2.__str__(),
+            comment='Initial version.',
+            user='testuser',
+            content_type=self.ct
+        )
+
+    @patch('Poem.api.internal_views.thresholdsprofiles.sync_webapi')
+    def test_get_all_thresholds_profiles(self, func):
+        func.side_effect = mocked_func
+        request = self.factory.get(self.url)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        self.assertEqual(
+            response.data,
+            [
+                OrderedDict([
+                    ('name', 'ANOTHER_PROFILE'),
+                    ('description', ''),
+                    ('apiid', '12341234-oooo-kkkk-aaaa-aaeekkccnnee'),
+                    ('groupname', '')
+                ]),
+                OrderedDict([
+                    ('name', 'TEST_PROFILE'),
+                    ('description', ''),
+                    ('apiid', '00000000-oooo-kkkk-aaaa-aaeekkccnnee'),
+                    ('groupname', 'GROUP')
+                ])
+            ]
+        )
+
+    @patch('Poem.api.internal_views.thresholdsprofiles.sync_webapi')
+    def test_get_thresholds_profiles_if_no_authentication(self, func):
+        func.side_effect = mocked_func
+        request = self.factory.get(self.url)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @patch('Poem.api.internal_views.thresholdsprofiles.sync_webapi')
+    def test_get_thresholds_profile_by_name(self, func):
+        func.side_effect = mocked_func
+        request = self.factory.get(self.url + 'TEST_PROFILE')
+        force_authenticate(request, user=self.user)
+        response = self.view(request, 'TEST_PROFILE')
+        self.assertEqual(
+            response.data,
+            OrderedDict([
+                ('name', 'TEST_PROFILE'),
+                ('description', ''),
+                ('apiid', '00000000-oooo-kkkk-aaaa-aaeekkccnnee'),
+                ('groupname', 'GROUP')
+            ])
+        )
+
+    @patch('Poem.api.internal_views.thresholdsprofiles.sync_webapi')
+    def test_get_thresholds_profile_by_nonexisting_name(self, func):
+        func.side_effect = mocked_func
+        request = self.factory.get(self.url + 'nonexisting')
+        force_authenticate(request, user=self.user)
+        response = self.view(request, 'nonexisting')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(
+            response.data,
+            {'detail': 'Thresholds profile not found.'}
+        )
+
+    def test_put_thresholds_profile(self):
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        data = {
+            'name': 'NEW_TEST_PROFILE',
+            'apiid': '00000000-oooo-kkkk-aaaa-aaeekkccnnee',
+            'groupname': 'NEWGROUP',
+            'rules': json.dumps([
+                {
+                    'host': 'newHost',
+                    'metric': 'newMetric',
+                    'thresholds': 'entries=1;3;0:2;10'
+                }
+            ])
+        }
+        content, content_type = encode_data(data)
+        request = self.factory.put(self.url, content, content_type=content_type)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        tp = poem_models.ThresholdsProfiles.objects.get(id=self.tp1.id)
+        self.assertEqual(tp.name, 'NEW_TEST_PROFILE')
+        self.assertEqual(tp.groupname, 'NEWGROUP')
+        group1 = poem_models.GroupOfThresholdsProfiles.objects.get(
+            name='NEWGROUP'
+        )
+        group2 = poem_models.GroupOfThresholdsProfiles.objects.get(
+            name='GROUP'
+        )
+        self.assertTrue(
+            group1.thresholdsprofiles.filter(
+                apiid='00000000-oooo-kkkk-aaaa-aaeekkccnnee'
+            ).exists()
+        )
+        self.assertFalse(
+            group2.thresholdsprofiles.filter(
+                apiid='00000000-oooo-kkkk-aaaa-aaeekkccnnee'
+            ).exists()
+        )
+        history = poem_models.TenantHistory.objects.filter(
+            object_id=tp.id, content_type=self.ct
+        ).order_by('-date_created')
+        self.assertEqual(history.count(), 2)
+        comment_set = set()
+        for item in json.loads(history[0].comment):
+            comment_set.add(json.dumps(item))
+        self.assertEqual(
+            comment_set,
+            {'{"changed": {"fields": ["groupname", "name", "rules"]}}'}
+        )
+        serialized_data = json.loads(history[0].serialized_data)[0]['fields']
+        self.assertEqual(serialized_data['name'], tp.name)
+        self.assertEqual(serialized_data['groupname'], tp.groupname)
+        self.assertEqual(serialized_data['apiid'], tp.apiid)
+        self.assertEqual(
+            serialized_data['rules'],
+            json.dumps([
+                {
+                    'host': 'newHost',
+                    'metric': 'newMetric',
+                    'thresholds': 'entries=1;3;0:2;10'
+                }
+            ])
+        )
+
+    def test_put_thresholds_profile_with_invalid_data(self):
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        data = {
+            'name': 'NEW_TEST_PROFILE',
+            'apiid': '',
+            'groupname': 'GROUP',
+            'rules': json.dumps([
+                {
+                    'host': 'newHost',
+                    'metric': 'newMetric',
+                    'thresholds': 'entries=1;3;0:2;10'
+                }
+            ])
+        }
+        content, content_type = encode_data(data)
+        request = self.factory.put(self.url, content, content_type=content_type)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'detail': 'Apiid field undefined!'})
+
+    def test_post_thresholds_profile(self):
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        data = {
+            'name': 'NEW_PROFILE',
+            'apiid': '12341234-aaaa-kkkk-aaaa-aaeekkccnnee',
+            'groupname': 'GROUP',
+            'rules': json.dumps([
+                {
+                    'host': 'newHost',
+                    'metric': 'newMetric',
+                    'thresholds': 'entries=1;3;0:2;10'
+                }
+            ])
+        }
+        request = self.factory.post(self.url, data, format='json')
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 3
+        )
+        profile = poem_models.ThresholdsProfiles.objects.get(
+            apiid='12341234-aaaa-kkkk-aaaa-aaeekkccnnee'
+        )
+        self.assertEqual(profile.name, 'NEW_PROFILE')
+        self.assertEqual(profile.groupname, 'GROUP')
+        group = poem_models.GroupOfThresholdsProfiles.objects.get(name='GROUP')
+        self.assertTrue(
+            group.thresholdsprofiles.filter(
+                apiid='12341234-aaaa-kkkk-aaaa-aaeekkccnnee'
+            ).exists()
+        )
+        history = poem_models.TenantHistory.objects.filter(
+            object_id=profile.id, content_type=self.ct
+        ).order_by('-date_created')
+        self.assertEqual(history.count(), 1)
+        self.assertEqual(history[0].comment, 'Initial version.')
+        serialized_data = json.loads(history[0].serialized_data)[0]['fields']
+        self.assertEqual(serialized_data['name'], profile.name)
+        self.assertEqual(serialized_data['apiid'], profile.apiid)
+        self.assertEqual(serialized_data['groupname'], profile.groupname)
+        self.assertEqual(
+            serialized_data['rules'],
+            '[{"host": "newHost", "metric": "newMetric", '
+            '"thresholds": "entries=1;3;0:2;10"}]'
+        )
+
+    def test_post_thresholds_profile_with_invalid_data(self):
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        data = {
+            'name': 'NEW_PROFILE',
+            'apiid': '',
+            'groupname': 'GROUP',
+            'rules': json.dumps([
+                {
+                    'host': 'newHost',
+                    'metric': 'newMetric',
+                    'thresholds': 'entries=1;3;0:2;10'
+                }
+            ])
+        }
+        request = self.factory.post(self.url, data, format='json')
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data,
+            {
+                'detail': 'apiid: This field may not be blank.'
+            }
+        )
+
+    def test_delete_thresholds_profile(self):
+        self.assertEqual(
+            poem_models.ThresholdsProfiles.objects.all().count(), 2
+        )
+        self.assertEqual(
+            poem_models.TenantHistory.objects.filter(
+                object_id=self.tp1.id, content_type=self.ct
+            ).count(), 1
+        )
+        request = self.factory.delete(
+            self.url + '00000000-oooo-kkkk-aaaa-aaeekkccnnee'
+        )
+        force_authenticate(request, user=self.user)
+        response = self.view(request, '00000000-oooo-kkkk-aaaa-aaeekkccnnee')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertRaises(
+            poem_models.ThresholdsProfiles.DoesNotExist,
+            poem_models.ThresholdsProfiles.objects.get,
+            id=self.tp1.id
+        )
+        self.assertEqual(
+            poem_models.TenantHistory.objects.filter(
+                object_id=self.tp1.id, content_type=self.ct
+            ).count(), 0
+        )
+
+    def test_delete_nonexisting_thresholds_profile(self):
+        request = self.factory.delete(self.url + 'nonexisting')
+        force_authenticate(request, user=self.user)
+        response = self.view(request, 'nonexisting')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(
+            response.data, {'detail': 'Thresholds profile not found.'}
+        )
+
+    def test_delete_thresholds_profile_without_specifying_apiid(self):
+        request = self.factory.delete(self.url)
+        force_authenticate(request, user=self.user)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data, {'detail': 'Thresholds profile not specified!'}
+        )
