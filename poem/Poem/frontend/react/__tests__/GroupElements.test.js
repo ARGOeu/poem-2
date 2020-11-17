401ca19479101ca10c416e89009ec62e2ecdb6be
Katarina Zailac <katarina.zailac@gmail.com>
2020-11-17 13:23:42 +0100

    test render empty table

--- poem/Poem/frontend/react/__tests__/GroupElements.test.js
+++ poem/Poem/frontend/react/__tests__/GroupElements.test.js
@@ -4,6 +4,7 @@ import { createMemoryHistory } from 'history';
 import { Route, Router } from 'react-router-dom';
 import { GroupList } from '../GroupElements';
 import { render, waitFor, screen } from '@testing-library/react';
+import { Backend } from '../DataManager';
 
 
 const mockListGroups = {
@@ -133,6 +134,39 @@ describe('Tests for groups listviews', () => {
     expect(screen.getByRole('row', {name: /group2/i}).textContent).toBe('2thresholdsgroup2')
     expect(screen.getByRole('link', {name: /1/i})).toHaveProperty('href', 'http://localhost/ui/administration/groupofthresholdsprofiles/thresholdsgroup1')
     expect(screen.getByRole('link', {name: /2/i})).toHaveProperty('href', 'http://localhost/ui/administration/groupofthresholdsprofiles/thresholdsgroup2')
+    expect(screen.getByRole('button', {name: 'Add'})).toBeTruthy()
+  })
+
+  test('Render empty table properly', async () => {
+    Backend.mockImplementation(() => {
+      return {
+        fetchResult: () => Promise.resolve({
+          'aggregations': ['aggrgroup1', 'aggrgroup2'],
+          'metrics': [],
+          'metricprofiles': ['metricprofilegroup1', 'metricprofilegroup2'],
+          'thresholdsprofiles': ['thresholdsgroup1', 'thresholdsgroup2']
+        })
+      }
+    })
+
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={history}>
+        <Route render={props => <GroupList {...props} group='metrics' id='groupofmetrics' name='group of metrics' />} />
+      </Router>
+    )
+    expect(screen.getByText(/loading/i).textContent).toBe('Loading data...');
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', {name: /group/i}).textContent).toBe('Select group of metrics to change')
+    })
+    expect(screen.getAllByRole('columnheader')).toHaveLength(2)
+    expect(screen.getByRole('columnheader', {name: /group/i}).textContent).toBe('Group of metrics')
+    expect(screen.getByRole('columnheader', {name: /#/i}).textContent).toBe('#')
+    expect(screen.getAllByRole('row')).toHaveLength(11)
+    expect(screen.getAllByRole('row', {name: ''})).toHaveLength(9)
+    expect(screen.getByRole('row', {name: /no/i}).textContent).toBe('No groups')
     expect(screen.getByRole('button', {name: 'Add'})).toBeTruthy()
   })
 })
