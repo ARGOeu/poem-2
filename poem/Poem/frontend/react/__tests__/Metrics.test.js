fa90a3ce88424e4086f9fd92bb7950ece0b85beb
Katarina Zailac <katarina.zailac@gmail.com>
2020-12-01 10:25:32 +0100

    test that cheks if list of metrics renders properly

---
+++ poem/Poem/frontend/react/__tests__/Metrics.test.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { createMemoryHistory } from 'history';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Route, Router } from 'react-router-dom';
+import { ListOfMetrics } from '../Metrics';
+
+
+const mockListOfMetrics = [
+  {
+    id: 1,
+    name: 'argo.AMS-Check',
+    mtype: 'Active',
+    tags: ['test_tag1', 'test_tag2'],
+    probeversion: 'ams-probe (0.1.12)',
+    group: 'EGI',
+    description: 'Description of argo.AMS-Check metric',
+    parent: '',
+    probeexecutable: 'ams-probe',
+    config: [
+      { key: 'maxCheckAttempts', value: '3' },
+      { key: 'timeout', value: '60' },
+      { key: 'interval', value: '5' },
+      { key: 'retryInterval', value: '3' },
+      { key: 'path', value: '/usr/libexec/argo-monitoring/probes/argo' }
+    ],
+    attribute: [
+      { key: 'argo.ams_TOKEN', value: '--token' }
+    ],
+    dependancy: '',
+    flags: [
+      { key: 'OBSESS', value: '1' }
+    ],
+    parameter: [
+      { key: '--project', value: 'EGI' }
+    ],
+    fileparameter: []
+  },
+  {
+    id: 3,
+    name: 'argo.AMS-Publisher',
+    mtype: 'Active',
+    tags: ['internal'],
+    probeversion: 'ams-publisher-probe (0.1.12)',
+    group: 'EGI',
+    description: '',
+    parent: '',
+    probeexecutable: 'ams-publisher-probe',
+    config: [
+      { key: 'interval', value: '180' },
+      { key: 'maxCheckAttempts', value: '1' },
+      { key: 'path', value: '/usr/libexec/argo-monitoring/probes/argo'},
+      { key: 'retryInterval', value: '1' },
+      { key: 'timeout', value: '120' }
+    ],
+    attribute: [],
+    dependancy: [],
+    flags: [
+      { key: 'NOHOSTNAME', value: '1' },
+      { key: 'NOTIMEOUT', value: '1'},
+      { key: 'NOPUBLISH', value: '1' }
+    ],
+    files: [],
+    parameter: [
+      { key: '-s', value: '/var/run/argo-nagios-ams-publisher/sock'}
+    ],
+    fileparameter: []
+  },
+  {
+    id: 2,
+    name: 'org.apel.APEL-Pub',
+    mtype: 'Passive',
+    tags: [],
+    probeversion: '',
+    group: 'ARGOTEST',
+    description: '',
+    parent: '',
+    probeexecutable: '',
+    config: [],
+    attribute: [],
+    dependancy: [],
+    flags: [
+      { key: 'OBSESS', value: '1' },
+      { key: 'PASSIVE', value: '1' }
+    ],
+    files: [],
+    parameter: [],
+    fileparameter: []
+  }
+];
+
+jest.mock('../DataManager', () => {
+  return {
+    Backend: jest.fn().mockImplementation(() => {
+      return {
+        fetchData: (path) => {
+          switch (path) {
+            case '/api/v2/internal/mtypes':
+              return Promise.resolve(['Active', 'Passive'])
+
+            case '/api/v2/internal/metrictags':
+              return Promise.resolve(['test_tag1', 'test_tag2', 'internal'])
+
+            case '/api/v2/internal/metric':
+              return Promise.resolve(mockListOfMetrics)
+          }
+        },
+        fetchResult: () => Promise.resolve({
+          'aggregations': ['EGI'],
+          'metrics': ['EGI', 'ARGOTEST'],
+          'metricprofiles': ['EGI'],
+          'thresholdsprofiles': ['EGI']
+        }),
+        isTenantSchema: () => Promise.resolve(true),
+        isActiveSession: () => Promise.resolve({
+          active: true,
+          userdetails: {
+            is_superuser: false,
+            username: 'testuser',
+            groups: {
+              aggregations: ['EGI'],
+              metricprofiles: ['EGI'],
+              metrics: ['EGI'],
+              thresholdsprofiles: ['EGI']
+            },
+            token: '1234token_rw'
+          }
+        })
+      }
+    })
+  }
+})
+
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+
+function renderListView() {
+  const history = createMemoryHistory();
+
+  return {
+    ...render(
+      <Router history={history}>
+        <Route render={props => <ListOfMetrics {...props} type='metrics' />} />
+      </Router>
+    )
+  }
+}
+
+
+describe('Tests for metrics listview', () => {
+  test('Test that metrics listview renders properly', async () => {
+    renderListView();
+
+    expect(screen.getByText(/loading/i).textContent).toBe('Loading data...');
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', {name: /metric/i}).textContent).toBe('Select metric for details')
+    })
+    // double column header length because search fields are also th
+    expect(screen.getAllByRole('columnheader')).toHaveLength(12);
+    expect(screen.getByRole('columnheader', { name: '#' })).toBeInTheDocument();
+    expect(screen.getByRole('columnheader', { name: /name/i }).textContent).toBe('Name');
+    expect(screen.getByRole('columnheader', { name: /probe/i }).textContent).toBe('Probe version');
+    expect(screen.getByRole('columnheader', { name: /type/i }).textContent).toBe('Type');
+    expect(screen.getByRole('columnheader', { name: /group/i }).textContent).toBe('Group');
+    expect(screen.getByRole('columnheader', { name: /tag/i }).textContent).toBe('Tag');
+    expect(screen.getAllByPlaceholderText('Search')).toHaveLength(2);
+    expect(screen.getAllByRole('columnheader', { name: 'Show all' })).toHaveLength(3);
+    expect(screen.getAllByRole('option', { name: 'Show all' })).toHaveLength(3);
+    expect(screen.getByRole('option', { name: /active/i })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: /passive/i })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: /egi/i })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: /argotest/i })).toBeInTheDocument();
+    expect(screen.getByRole('option', { name: /internal/i })).toBeInTheDocument();
+    expect(screen.getAllByRole('option', { name: /test_tag/i })).toHaveLength(2);
+    expect(screen.getAllByRole('row')).toHaveLength(52);
+    expect(screen.getAllByRole('row', { name: '' })).toHaveLength(47);
+    expect(screen.getByRole('row', { name: /ams-check/i }).textContent).toBe('1argo.AMS-Checkams-probe (0.1.12)ActiveEGItest_tag1test_tag2')
+    expect(screen.getByRole('row', { name: /ams-publisher/i }).textContent).toBe('2argo.AMS-Publisherams-publisher-probe (0.1.12)ActiveEGIinternal')
+    expect(screen.getByRole('row', { name: /apel/i }).textContent).toBe('3org.apel.APEL-PubPassiveARGOTESTnone')
+    expect(screen.getByRole('link', { name: /argo.ams-check/i }).closest('a')).toHaveAttribute('href', '/ui/metrics/argo.AMS-Check');
+    expect(screen.getByRole('link', { name: /ams-probe/i }).closest('a')).toHaveAttribute('href', '/ui/probes/ams-probe/history/0.1.12')
+    expect(screen.getByRole('link', { name: /argo.ams-publisher/i }).closest('a')).toHaveAttribute('href', '/ui/metrics/argo.AMS-Publisher');
+    expect(screen.getByRole('link', { name: /ams-publisher-probe/i }).closest('a')).toHaveAttribute('href', '/ui/probes/ams-publisher-probe/history/0.1.12')
+    expect(screen.getByRole('link', { name: /apel/i }).closest('a')).toHaveAttribute('href', '/ui/metrics/org.apel.APEL-Pub');
+    expect(screen.queryByText(/add/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/import/i)).not.toBeInTheDocument();
+  })
+})
