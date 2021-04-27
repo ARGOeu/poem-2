8f702e254a367f508fb90e6f55b7651c4abf4018
Katarina Zailac <katarina.zailac@gmail.com>
2021-04-27 13:06:01 +0200

    tighten up GET method for api keys

--- poem/Poem/api/internal_views/apikey.py
+++ poem/Poem/api/internal_views/apikey.py
@@ -2,6 +2,7 @@ import datetime
 
 from Poem.api.models import MyAPIKey
 from Poem.api.views import NotFound
+from Poem.poem import models as poem_models
 from django.db.models import Q
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
@@ -15,31 +16,71 @@ class ListAPIKeys(APIView):
     def get(self, request, name=None):
         if name:
             try:
-                apikey = MyAPIKey.objects.get(name=name)
-                api_format = dict(
-                    id=apikey.id,
-                    name=apikey.name,
-                    token=apikey.token,
-                    created=datetime.datetime.strftime(apikey.created,
-                                                       '%Y-%m-%d %H:%M:%S'),
-                    revoked=apikey.revoked
+                userprofile = poem_models.UserProfile.objects.get(
+                    user=request.user
                 )
+                regular_user = not request.user.is_superuser and (
+                    len(userprofile.groupsofaggregations.all()) > 0 or
+                    len(userprofile.groupsofmetricprofiles.all()) > 0 or
+                    len(userprofile.groupsofthresholdsprofiles.all()) > 0
+                )
+                regular_user_no_perms = not request.user.is_superuser and (
+                    len(userprofile.groupsofaggregations.all()) == 0 and
+                    len(userprofile.groupsofmetricprofiles.all()) == 0 and
+                    len(userprofile.groupsofthresholdsprofiles.all()) == 0
+                )
+                if request.user.is_superuser or (
+                    regular_user and name.startswith('WEB-API')
+                ) or (
+                    regular_user_no_perms and name == 'WEB-API-RO'
+                ):
+                    apikey = MyAPIKey.objects.get(name=name)
+                    api_format = dict(
+                        id=apikey.id,
+                        name=apikey.name,
+                        token=apikey.token,
+                        created=datetime.datetime.strftime(
+                            apikey.created, '%Y-%m-%d %H:%M:%S'
+                        ),
+                        revoked=apikey.revoked
+                    )
+
+                else:
+                    return Response(
+                        {
+                            'detail': 'You do not have permission for fetching '
+                                      'this API key.'
+                        },
+                        status=status.HTTP_401_UNAUTHORIZED
+                    )
 
             except MyAPIKey.DoesNotExist:
                 raise NotFound(status=404, detail='API key not found')
+
+            except poem_models.UserProfile.DoesNotExist:
+                raise NotFound(
+                    status=404,
+                    detail='User profile for authenticated user not found.'
+                )
 
         else:
             if request.user.is_superuser:
                 apikeys = MyAPIKey.objects.all().order_by('name')
             else:
-                apikeys = MyAPIKey.objects.filter(name__startswith='WEB-API').order_by('name')
+                apikeys = MyAPIKey.objects.filter(
+                    name__startswith='WEB-API'
+                ).order_by('name')
+
             api_format = [
-                dict(id=e.id,
-                     name=e.name,
-                     token=e.token,
-                     created=datetime.datetime.strftime(e.created,
-                                                        '%Y-%m-%d %H:%M:%S'),
-                     revoked=e.revoked) for e in apikeys]
+                dict(
+                    id=e.id,
+                    name=e.name,
+                    created=datetime.datetime.strftime(
+                        e.created, '%Y-%m-%d %H:%M:%S'
+                    ),
+                    revoked=e.revoked
+                ) for e in apikeys
+            ]
 
         return Response(api_format)
 
