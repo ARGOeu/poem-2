68e8752e988341fc6072790b8c59a454764f5785
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-07-20 11:48:21 +0200

    initial Dockerfile and docker-run script

---
+++ testenv/docker-run-poem-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+test -z $1 && TAG="latest" || TAG="$1"
+
+docker run \
+--log-driver json-file \
+--log-opt max-size=10m \
+-v /dev/log:/dev/log \
+-v /etc/localtime:/etc/localtime \
+-v $HOME:/mnt/ \
+-v $HOME/.ssh:/home/user/.ssh/ \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-h docker-centos7 \
+--net host \
+--name poem-react-tests \
+--rm -ti \
+poem-react-tests:$TAG
