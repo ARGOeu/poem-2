d5aa365317fd9ea221f8117b3d476cc3b4797b3f
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-07-22 20:54:01 +0200

    helper shell scripts

---
+++ testenv/poem-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# load virtual environment
+source /etc/profile.d/venv_poem.sh
+workon poem
+
+# build and install wheel
+cd /mnt/poem-source
+make clean
+make wheel-devel
+sudo pip3 install dist/*.whl
+sudo pip3 install -r requirements.txt
+sudo pip3 install -r requirements_tests.txt
+
+# prerequisites
+sudo cp -f /home/user/secret_key $VIRTUAL_ENV/etc/poem/secret_key
+sudo cp -f /home/user/poem.conf $VIRTUAL_ENV/etc/poem/poem.conf
