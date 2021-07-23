76d2a7a1bd7372d843951a5d2fce6ae5290deee0
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-07-23 15:10:31 +0200

    leave source argument

--- testenv/execute-backend-tests.sh
+++ testenv/execute-backend-tests.sh
@@ -4,7 +4,7 @@
 source /etc/profile.d/venv_poem.sh
 workon poem
 
-cd /mnt/poem-source/
-
-sudo coverage run --source='.' /home/pyvenv/poem/lib/python3.6/site-packages/Poem/manage.py test api
+sudo coverage run /home/pyvenv/poem/lib/python3.6/site-packages/Poem/manage.py test api
 sudo coverage xml
+
+mv -f coverage.xml /mnt/poem-source
