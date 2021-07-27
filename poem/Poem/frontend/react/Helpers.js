802d7d70f48ca6d96b6468700b843bc270f172d6
Katarina Zailac <katarina.zailac@gmail.com>
2021-07-27 11:17:39 +0200

    added helper function for downloading CSV files

--- poem/Poem/frontend/react/Helpers.js
+++ poem/Poem/frontend/react/Helpers.js
@@ -1,7 +1,21 @@
-export function downloadFile(content, filename) {
+export function downloadJSON(content, filename) {
+  return downloadFile(content, filename, 'json')
+}
+
+
+export function downloadCSV(content, filename) {
+  return downloadFile(content, filename, 'csv')
+}
+
+
+function downloadFile(content, filename, type) {
   const link = document.createElement('a');
-  const blob = new Blob([JSON.stringify(content, null, 2)], { type: 'application/json' });
-  link.setAttribute('href', URL.createObjectURL(blob));
+  if (type === 'json') {
+    const blob = new Blob([JSON.stringify(content, null, 2)], { type: 'application/json' });
+    link.setAttribute('href', URL.createObjectURL(blob));
+  } else{
+    link.setAttribute('href', encodeURI(`data:text/csv;charset=utf8,\ufeff${content}`));
+  }
   link.setAttribute('download', filename)
   link.click();
   link.remove();
