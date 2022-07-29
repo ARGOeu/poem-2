04258bd753c30d6fe8bc560e8a8cabaa777c3191
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-07-29 11:34:33 +0200

    fix webpack config after update and load sass-loader also

--- poem/Poem/webpack.config.js
+++ poem/Poem/webpack.config.js
@@ -35,11 +35,24 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader', {
-            loader: 'postcss-loader',
+        use: ['style-loader','css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
             options: {
-              plugins: () => [require('autoprefixer')]
-            }}]
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          }
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/i,
