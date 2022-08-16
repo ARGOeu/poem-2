6a91443732e15fc03a007ff1a448d8fd6ed902d3
Katarina Zailac <katarina.zailac@gmail.com>
2022-08-16 13:44:58 +0200

    Remove redundant fields from the Metric model

--- poem/Poem/poem/dbmodels/metricstags.py
+++ poem/Poem/poem/dbmodels/metricstags.py
@@ -47,22 +47,10 @@ class GroupOfMetrics(models.Model):
 class Metric(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=128, unique=True)
-    mtype = models.ForeignKey(MetricType, on_delete=models.CASCADE)
     probeversion = models.CharField(max_length=1024, null=True, blank=True)
-    probekey = models.ForeignKey(ProbeHistory, blank=True, null=True,
-                                 on_delete=models.SET_NULL)
-    description = models.TextField(default='')
     group = models.ForeignKey(GroupOfMetrics, null=True,
                               on_delete=models.SET_NULL)
-    parent = models.CharField(max_length=128)
-    probeexecutable = models.CharField(max_length=128)
     config = models.CharField(max_length=1024)
-    attribute = models.CharField(max_length=1024)
-    dependancy = models.CharField(max_length=1024)
-    flags = models.CharField(max_length=1024)
-    files = models.CharField(max_length=1024)
-    parameter = models.CharField(max_length=1024)
-    fileparameter = models.CharField(max_length=1024)
 
     class Meta:
         permissions = (('metricsown', 'Read/Write/Modify'),)
