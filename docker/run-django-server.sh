90cec61e31e5f4184ff3a0c433fa8090a8e26a52
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-09-25 21:56:11 +0200

    fix script for it correctly takes pid args

--- docker/run-django-server.sh
+++ docker/run-django-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RUNASROOT="su -m -s /bin/bash root -c"
-$RUNASROOT "kill -9 `pgrep -f runserver` >/dev/null"
+PIDS=$(pgrep -f runserver | tr '\n' ' ')
+$RUNASROOT "kill -9 $PIDS 2>/dev/null"
 
 $RUNASROOT ". /home/pyvenv/poem/bin/activate && poem-manage runserver 0.0.0.0:8000"
