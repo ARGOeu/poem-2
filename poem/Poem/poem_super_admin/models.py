c74d9bdf1a93db22b22fa4a2fe6d51804f27e88d
Katarina Zailac <katarina.zailac@gmail.com>
2023-03-07 10:08:56 +0100

    Created new WebAPIKey model in super POEM

--- poem/Poem/poem_super_admin/models.py
+++ poem/Poem/poem_super_admin/models.py
@@ -2,3 +2,4 @@ from Poem.poem_super_admin.dbmodels.history import *
 from Poem.poem_super_admin.dbmodels.yumrepos import *
 from Poem.poem_super_admin.dbmodels.probes import *
 from Poem.poem_super_admin.dbmodels.metrictemplates import *
+from Poem.poem_super_admin.dbmodels.apikey import *
