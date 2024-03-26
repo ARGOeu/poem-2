2fc39ed3ff01bd2399364740a9d8e61299825d07
Katarina Zailac <katarina.zailac@gmail.com>
2024-03-26 13:56:59 +0100

    Improve placeholders for metric overrides' views

--- poem/Poem/frontend/react/MetricOverrides.js
+++ poem/Poem/frontend/react/MetricOverrides.js
@@ -7,20 +7,20 @@ import {
   BaseArgoTable,
   BaseArgoView,
   ErrorComponent,
-  LoadingAnim,
   NotifyError,
   NotifyOk
 } from "./UIElements"
 import {
+  Button,
+  Col,
+  Form,
   FormGroup,
+  FormFeedback,
+  Input,
   InputGroup,
   InputGroupText,
   Row,
-  Col,
-  Button,
-  Form,
-  Input,
-  FormFeedback
+  Table
 } from "reactstrap"
 import { ParagraphTitle } from "./UIElements"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -34,7 +34,11 @@ import {
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from "yup";
 import { ErrorMessage } from "@hookform/error-message"
-import { CustomButton, CustomHeadline, CustomInput, CustomProfilesList, CustomSubtitle, CustomTable } from "./Placeholders"
+import { 
+  ChangeViewPlaceholder, 
+  InputPlaceholder, 
+  ListViewPlaceholder 
+} from "./Placeholders"
 
 const hostnameRegex = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9])([_][A-Za-z0-9.\-_]*)*$/
 
@@ -160,7 +164,11 @@ export const MetricOverrideList = () => {
   ])
 
   if (loadingUserDetails || loadingConfs)
-    return (<CustomProfilesList />)
+    return (
+      <ListViewPlaceholder
+        resourcename="metric configuration override"
+      />
+    )
 
   else if (errorUserDetails)
     return (<ErrorComponent error={errorUserDetails} />)
@@ -791,29 +799,28 @@ export const MetricOverrideChange = (props) => {
 
   if (loading || loadingOverride)
     return (
-      <>
-        <CustomHeadline width="557px" height="38.4px"/>
-        <Form className='ms-2 mb-2 mt-2 p-3 border placeholder-glow rounded d-flex flex-column'>
-          <CustomInput height="48px" width="50%" custStyle="mb-3" />
-          <CustomSubtitle height="37.6px" />
-          <CustomTable height="80px" custStyle="mb-3" />
-          <CustomSubtitle height="37.6px" />
-          <CustomTable height="80px" custStyle="mb-3" />
-          <CustomSubtitle height="37.6px" />
-          <CustomTable height="80px" />
-
-          {window.location.pathname === "/ui/administration/metricoverrides/add" ? 
-            <div className='ms-2 mb-2 mt-4 p-3 border placeholder-glow rounded d-flex justify-content-end'>
-              <CustomButton height="37.6px" width="60px" />
-            </div>
-            :
-            <div className='ms-2 mb-2 mt-4 p-3 border placeholder-glow rounded d-flex justify-content-between'>
-              <CustomButton height="37.6px" width="100px" />
-              <CustomButton height="37.6px" width="60px" />
-            </div>
-          }
-        </Form>
-      </>
+      <ChangeViewPlaceholder
+        resourcename="metric configuration override"
+        addview={ addview }
+      >
+        <FormGroup>
+          <Row>
+            <Col md={6}>
+              <InputPlaceholder width="100%" />
+            </Col>
+          </Row>
+        </FormGroup>
+        <FormGroup>
+          <ParagraphTitle title="Global attributes" />
+          <Table className="placeholder rounded" style={{ height: "200px" }} />
+          <ParagraphTitle title="Host attributes" />
+          <Table className="placeholder rounded" style={{ height: "200px" }} />
+        </FormGroup>
+        <FormGroup>
+          <ParagraphTitle title="Metrics' parameters" />
+          <Table className="placeholder rounded" style={{ height: "200px" }} />
+        </FormGroup>
+      </ChangeViewPlaceholder>
     )
 
   else if (errorOverride)
