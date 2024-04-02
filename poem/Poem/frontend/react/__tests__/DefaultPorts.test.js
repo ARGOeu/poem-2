68eda2e755093ab0b30bbbb4d5bcdd4571515903
Katarina Zailac <katarina.zailac@gmail.com>
2024-04-02 08:58:46 +0200

    Fixed tests for default ports

--- poem/Poem/frontend/react/__tests__/DefaultPorts.test.js
+++ poem/Poem/frontend/react/__tests__/DefaultPorts.test.js
@@ -6,7 +6,7 @@ import {
   waitFor,
   within
 } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { DefaultPortsList } from '../DefaultPorts';
 import { Backend } from '../DataManager';
@@ -94,8 +94,10 @@ describe("Test default ports list", () => {
     renderView();
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     const table = within(screen.getByRole("table"))
     expect(table.getAllByRole("columnheader")).toHaveLength(4)
@@ -122,8 +124,10 @@ describe("Test default ports list", () => {
     renderView();
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.change(screen.getAllByPlaceholderText(/search/i)[0], { target: { value: "BDII" } })
 
@@ -145,8 +149,10 @@ describe("Test default ports list", () => {
     renderView();
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.change(screen.getAllByPlaceholderText(/search/i)[1], { target: { value: "75" } })
 
@@ -167,8 +173,10 @@ describe("Test default ports list", () => {
     renderView();
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.click(screen.getByTestId("insert-1"))
 
@@ -194,8 +202,10 @@ describe("Test default ports list", () => {
     renderView();
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.click(screen.getByTestId("remove-1"))
 
@@ -217,8 +227,10 @@ describe("Test default ports list", () => {
     renderView()
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.click(screen.getByTestId("insert-1"))
 
@@ -259,8 +271,10 @@ describe("Test default ports list", () => {
     renderView()
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.click(screen.getByTestId("insert-1"))
 
@@ -305,8 +319,10 @@ describe("Test default ports list", () => {
     renderView()
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     fireEvent.click(screen.getByTestId("insert-1"))
 
@@ -355,8 +371,10 @@ describe("Test default ports list", () => {
     renderView();
 
     await waitFor(() => {
-      expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
+      expect(screen.getByText("Port name")).toBeInTheDocument()
     })
+
+    expect(screen.getByRole("heading", { name: /port/i }).textContent).toBe("Default ports");
 
     const table = within(screen.getByRole("table"))
     expect(table.getAllByRole("columnheader")).toHaveLength(4)
