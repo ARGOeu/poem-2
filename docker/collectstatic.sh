267194332b8e0d7b9489d8a371b897d71179cf07
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-06-24 22:54:39 +0200

    correct virtenv activate

--- docker/collectstatic.sh
+++ docker/collectstatic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /opt/poem/activate
+. /opt/poem/bin/activate
 rm -rf $VIRTUAL_ENV/usr/share/poem/static/reactbundle/* ; \
 python $VIRTUAL_ENV/lib/python3.9/site-packages/Poem/manage.py collectstatic --noinput ; \
 rm -rf $VIRTUAL_ENV/usr/share/poem/static/admin
