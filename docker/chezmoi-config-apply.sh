5e9c7c6b1e4388817586aaa38214f2dfa349c88e
Daniel Vrcic <daniel.vrcic@gmail.com>
2025-07-01 17:19:52 +0200

    add update chezmoi script

--- docker/chezmoi-config-apply.sh
+++ docker/chezmoi-config-apply.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
-bin/chezmoi init git@github.com:vrdel/dotfiles
-bin/chezmoi apply
-rm -rf .local/share/chezmoi
+let tokauthn=0
+let keyauthn=1
+
+usage()
+{
+  printf "Usage: %s [argument]\n" $(basename $0) >&2
+  printf "       [-t]        - Github token authn\n" >&2
+  printf "       [-k]        - Github SSH key authn (.ssh/id_rsa)\n" >&2
+  printf "       [-h]        - usage\n" >&2
+  exit 2
+}
+
+if [[ $# == 0 ]]
+then
+    usage
+fi
+
+while getopts 'tk' OPTION
+do
+    case $OPTION in
+        t)
+            tokauthn=1
+            ;;
+        k)
+            keyauthn=1
+            ;;
+        h)
+            usage
+            ;;
+        ?)
+            usage
+            ;;
+    esac
+done
+
+if [ "${tokauthn}" -gt 0 ]
+then
+  ssh-keyscan github.com >> .ssh/known_hosts
+  bin/chezmoi init https://github.com/vrdel/dotfiles
+  bin/chezmoi apply
+  rm -rf .local/share/chezmoi
+else
+  ssh-keyscan github.com >> .ssh/known_hosts
+  bin/chezmoi init git@github.com:vrdel/dotfiles
+  bin/chezmoi apply
+  rm -rf .local/share/chezmoi
+fi
+
