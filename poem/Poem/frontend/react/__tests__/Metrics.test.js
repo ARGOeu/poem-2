10aa7dc98f708be648b4af4e2cbe679e3cf15ed9
Daniel Vrcic <daniel.vrcic@gmail.com>
2026-05-28 11:02:13 +0200

    fix async metric group option assertions in Metrics tests

--- poem/Poem/frontend/react/__tests__/Metrics.test.js
+++ poem/Poem/frontend/react/__tests__/Metrics.test.js
@@ -845,7 +845,7 @@ describe('Tests for metric change', () => {
 
     expect(screen.queryByText('ARGOTEST')).not.toBeInTheDocument()
     selectEvent.openMenu(groupField)
-    expect(screen.getByText('ARGOTEST')).toBeInTheDocument()
+    expect(await screen.findByText('ARGOTEST')).toBeInTheDocument()
 
     expect(screen.getByRole('heading', { name: /metric configuration/i })).toBeInTheDocument()
 
@@ -967,7 +967,7 @@ describe('Tests for metric change', () => {
 
     expect(screen.queryByText('ARGOTEST')).not.toBeInTheDocument()
     selectEvent.openMenu(groupField)
-    expect(screen.getByText('ARGOTEST')).toBeInTheDocument()
+    expect(await screen.findByText('ARGOTEST')).toBeInTheDocument()
 
     expect(screen.getByRole('heading', { name: /metric configuration/i })).toBeInTheDocument()
 
@@ -1910,7 +1910,7 @@ describe('Tests for metric change', () => {
 
     expect(screen.queryByText('ARGOTEST')).not.toBeInTheDocument()
     selectEvent.openMenu(groupField)
-    expect(screen.getByText('ARGOTEST')).toBeInTheDocument()
+    expect(await screen.findByText('ARGOTEST')).toBeInTheDocument()
 
     expect(screen.getByRole('heading', { name: /metric conf/i }).textContent).toBe('Metric configuration');
     expect(screen.getByRole('heading', { name: /exec/i }).textContent).toBe('probe executable');
